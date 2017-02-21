55a5cf7a5df7f73f2f777f05b06160e496a305bb
Vinícius Daly Felizardo <felizardow@gmail.com>
2017-02-20 23:36:36 -0800

    Now working with DB

--- server/io-handle.js
+++ server/io-handle.js
@@ -9,22 +9,25 @@ var ioHandle = (mongo, mongo_uri) => (socket) => {
   });
   socket.on('listen-party', (party_code) => {
     if (!user) return
+    let handlePartyDb = (party, matches, games) => {
+      let info = {name: party.name, users: party.users, code: party_code,
+        loading: false};
+      socket.emit('party', {type: 'SET_DOWN_MAPPING',
+                            downMapping: party.downMapping});
+      socket.emit('party', {type: 'SET_GAMES', games});
+      socket.emit('party', {type: 'SET_MATCHES', matches});
+      socket.emit('party', {type: 'SET_INFO', info});
+      socket.join('party-' + party._id)
+    }
+    //Do Mongo DB request
     let db = mongo.connect(mongo_uri, (err, db) => {
-      let partiesCollection = db.collection('parties');
-      partiesCollection.findOne(ObjectId(party_code), (err, party) => {
-        let info = {name: party.name, users: party.users,
-          code: party_code, loading: false}
-        let matches = [{id: 'awjdjdaw', game_code: 'checkers', game_name: "Checkers",
-                        status: "Going on", players: ["felizardow", "vitorpfr"]},
-                       {id: 'poqweqep', game_code: 'chess', game_name: "Chess",
-                        status: "Finished", players: ["rafaelplonghi", "curvorj"]}]
-        let games = [{code: 'chess', name: 'Chess', maxPlayers: 2},
-                     {code: 'checkers', name: 'Checkers', maxPlayers: 2}]
-        let downMapping = {'chess': ['vitorpfr'], 'checkers': ['felizardow']}
-        socket.emit('party', {type: 'SET_DOWN_MAPPING', downMapping});
-        socket.emit('party', {type: 'SET_GAMES', games});
-        socket.emit('party', {type: 'SET_MATCHES', matches});
-        socket.emit('party', {type: 'SET_INFO', info});
+      db.collection('parties').findOne(ObjectId(party_code), (err, party) => {
+        db.collection('matches').find({party_id:  ObjectId(party_code)})
+          .toArray((err, matches) => {
+          db.collection('games').find().toArray((err, games) => {
+            handlePartyDb(party, matches, games);
+          })
+        })
       })
     });
   })
