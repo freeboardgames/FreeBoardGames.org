87b07c1c3346f0a7b131c669d9d964055380505b
Vinícius Daly Felizardo <felizardow@gmail.com>
2017-03-29 01:25:37 -0700

    Agora um cache descente

--- server/match-handle.js
+++ server/match-handle.js
@@ -1,9 +1,9 @@
 const ObjectId = require('mongodb').ObjectId;
-const NodeCache = require( "node-cache" );
-const matchCache = new NodeCache( { stdTTL: 30 * 60, checkperiod: 60 } );
+const cache = require('memory-cache');
 const _ = require('underscore');
 const checkersReducer = require('./games/checkerGameState.js').default;
 var LAST_DB = null;
+const CACHE_DURATION = 30 * 60 * 1000;
 
 function genericReducer (game_code, state, action) {
   if (game_code == 'checkers') {
@@ -11,14 +11,26 @@ function genericReducer (game_code, state, action) {
   }
 }
 
+function saveAfterExpire (match_code, match) {
+  if (LAST_DB) {
+    console.log('EXPIRED... SAVING');
+    LAST_DB.collection('matches').updateOne({_id: ObjectId(match_code)},
+      { $set: {log: match.log} },
+    (err, results) => {
+      if (err)
+        console.log(err)
+    });
+  }
+};
+
 matchJoinHandle = (socket, dispatchRoom, dispatch, db, user, match_code) => {
   LAST_DB = db;
   db.collection('matches').findOne(ObjectId(match_code), (err, match) => {
-    let match_cache = matchCache.get(match_code);
+    let match_cache = cache.get(match_code);
     if (match_cache) {
       match.log = match_cache.log;
     }
-    matchCache.set(match_code, match);
+    cache.put(match_code, match, CACHE_DURATION, saveAfterExpire);
     let current_state = undefined;
     if (match.log && match.log.length > 0) {
       current_state = match.log[0].state;
@@ -40,7 +52,7 @@ matchJoinHandle = (socket, dispatchRoom, dispatch, db, user, match_code) => {
 matchActionRequest = (socket, dispatchRoom, dispatch, db, user, match_code, action) => {
   LAST_DB = db;
   console.log('MATCH ACTION REQUEST');
-  let match = matchCache.get(match_code);
+  let match = cache.get(match_code);
   if (!match) {
     console.log('ERROR: ACTION CACHE EXPIRED :(');
     return;
@@ -59,18 +71,8 @@ matchActionRequest = (socket, dispatchRoom, dispatch, db, user, match_code, acti
   if (next_state && !_.isEqual(next_state, current_state)) {
     match.log.unshift({state: next_state, action: action});
     dispatchRoom('match-' + match_code, action);
-    matchCache.set(match_code, match);
+    cache.put(match_code, match, CACHE_DURATION, saveAfterExpire);
   }
 }
-
-/*matchCache.on("expired", (match_code, match) => {
-  console.log('EXPIRED... SAVING');
-  LAST_DB.collection('matches').updateOne({_id: ObjectId(match_code)},
-    { $set: {log: match.log} },
-  (err, results) => {
-    if (err)
-      console.log(err)
-  });
-});*/
 
 module.exports = { matchActionRequest, matchJoinHandle }
