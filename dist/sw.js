b0110b5667089d482745b9bfff4aa6ebe94905aa
felizardo <felizardow@gmail.com>
2017-07-04 01:53:50 -0700

    Push notifications DONE in the client :) 2

---
+++ dist/sw.js
@@ -0,0 +1,7 @@
+self.addEventListener('push', function(event) {
+  if (event.data) {
+    console.log('This push event has data: ', event.data.text());
+  } else {
+    console.log('This push event has no data.');
+  }
+});
