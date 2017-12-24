909b549591aab075a214feb9901c5041464f94c7
Vinicius Daly Felizardo <felizardow@gmail.com>
2017-12-24 02:11:34 -0200

    Removing old karma files

--- all-tests.js
+++
@@ -1,5 +0,0 @@
-//This is used as an entry point to webpack to generate the test bundle
-//that will be used by mocha
-var context = require.context('./src', true, /\.test\./);
-context.keys().forEach(context);
-module.exports = context;
