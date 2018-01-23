1fd456a3f61cff6a50c5e36b0b84e81dfe4d6059
Vinicius Felizardo <felizardow@gmail.com>
2018-01-22 21:41:54 -0800

    Fixing build

--- src/components/App/GameBar/AlertLayer.tsx
+++ src/components/App/GameBar/AlertLayer.tsx
@@ -2,13 +2,13 @@ import * as React from 'react';
 
 class AlertLayer extends React.Component<{}, {}> {
   render() {
-    const mainStyle = {
+    const mainStyle: React.CSSProperties = {
       position: 'absolute', left: 0, top: 0,
       background: 'rgba(255,255,255,.85)',
       right: 0, height: '100%', zIndex: 9001,
       display: 'block', textAlign: 'center',
     };
-    const alignStyle = {
+    const alignStyle: React.CSSProperties = {
       transform: 'translateX(-50%) translateY(-50%)',
       left: '50%', top: '50%', position: 'absolute',
     };
