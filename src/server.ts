92e9ed7c9f127f13c0dfe6f4217187c37a4cc0b6
Vinicius Felizardo <felizardow@gmail.com>
2018-03-10 23:11:57 -0800

    Updating turnato server to use latest boardgame io

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,7 @@ import * as Koa from 'koa';
 import * as KoaSend from 'koa-send';
 import * as KoaStatic from 'koa-static';
 import * as KoaHelmet from 'koa-helmet';
-const Server = require('boardgame.io/server');
+const { Server } = require('boardgame.io/server');
 import Chess from './components/games/chess/game';
 
 const HOST = '0.0.0.0';
@@ -11,15 +11,15 @@ const NODE_ENV = process.env.NODE_ENV;
 const PROD = NODE_ENV === 'production';
 const DEV = !PROD;
 
-const app = Server({ games: [Chess] });
+const server = Server({ games: [Chess] });
 
-app.use(KoaStatic('./dist'));
-app.use(KoaHelmet());
+server.app.use(KoaStatic('./dist'));
+server.app.use(KoaHelmet());
 
-app.use(async (ctx:any) => {
+server.app.use(async (ctx:any) => {
   await KoaSend(ctx, `./dist/index.html`); 
 });
 
-app.listen(PORT, HOST, () => {
+server.app.listen(PORT, HOST, () => {
   console.log(`Serving ${NODE_ENV} at: http://localhost:${PORT}/`);
 });
