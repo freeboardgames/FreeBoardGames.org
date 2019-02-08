10ba417a7c6248707be0bba2ac63c010520f116f
flamecoals <flamecoals@users.noreply.github.com>
2019-02-08 23:01:52 +0000

    Improving hit and miss markers

--- src/games/seabattle/media/SvgMiss.tsx
+++ src/games/seabattle/media/SvgMiss.tsx
@@ -2,18 +2,18 @@ import * as React from 'react';
 const SvgMiss = (props: any) => (
   <g>
     <line
-      x1="0"
-      y1="0"
-      x2="1"
-      y2="1"
+      x1="0.2"
+      y1="0.2"
+      x2="0.8"
+      y2="0.8"
       stroke="white"
       strokeWidth="0.05"
     />
     <line
-      x1="1"
-      y1="0"
-      x2="0"
-      y2="1"
+      x1="0.8"
+      y1="0.2"
+      x2="0.2"
+      y2="0.8"
       stroke="white"
       strokeWidth="0.05"
     />
