f32501781e272ae767c35d79810385053dc12416
Jason <me@jasonharrison.us>
2019-02-09 17:06:11 -0500

    Begin adding header to gamelayout

--- src/App/Game/GameLayout.tsx
+++ src/App/Game/GameLayout.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { GameOver } from './GameOver';
 import { GameDarkSublayout } from './GameDarkSublayout';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import Typography from '@material-ui/core/Typography';
 
 interface IGameLayoutProps {
   children?: React.ReactNode;
@@ -15,6 +16,39 @@ export class GameLayout extends React.Component<IGameLayoutProps, {}> {
     } else {
       return (
         <MuiThemeProvider>
+          <div
+            style={{
+              maxWidth: '500px',
+              marginLeft: 'auto',
+              marginRight: 'auto',
+              position: 'fixed',
+            }}
+          >
+            <a href="/">
+              <img
+                style={{
+                  maxWidth: '500px',
+                  marginLeft: 'auto',
+                  marginRight: 'auto',
+                }}
+                src="/logo/fbg_logo_white_48.png"
+                alt="FbG"
+              />
+            </a>
+            <Typography
+              variant="title"
+              gutterBottom={true}
+              style={{
+                marginTop: '16px',
+                marginLeft: 'auto',
+                marginRight: 'auto',
+                color: 'white',
+              }}
+            >
+              FreeBoardGame.org
+            </Typography>
+            <div style={{ height: '64px' }} />
+          </div>
           <GameDarkSublayout>
             {this.props.children}
           </GameDarkSublayout>
