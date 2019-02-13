5ba5d0742f9562055f3ce8b29528f9599b30a671
flamecoals <flamecoals@users.noreply.github.com>
2019-02-13 07:00:28 -0800

    Trying to enable tree-shaking, organizing webpack config

---
+++ webpack.dev.js
@@ -0,0 +1,21 @@
+const merge = require('webpack-merge');
+const common = require('./webpack.common.js');
+const WebpackShellPlugin = require('webpack-shell-plugin-next');
+
+module.exports = merge(common, {
+  mode: 'development',
+  devtool: 'inline-source-map',
+  watchOptions: {
+    aggregateTimeout: 300,
+    poll: 1000
+  },
+  plugins: [
+    new WebpackShellPlugin({
+       onBuildEnd: {
+         scripts: ['node build/server.js'],
+         blocking: false,
+         parallel: true,
+       }
+    })
+  ]
+});
