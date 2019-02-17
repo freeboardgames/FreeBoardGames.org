be928badfea7dd293047e706ba7e2418095a947d
flamecoals <flamecoals@users.noreply.github.com>
2019-02-17 09:43:59 -0800

    Rehydrating component on the client

--- src/app.tsx
+++ src/app.tsx
@@ -1,11 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'babel-polyfill';
+import { AsyncComponentProvider, createAsyncContext } from 'react-async-component';
+import asyncBootstrapper from 'react-async-bootstrapper';
 import App from './App/App';
 import { BrowserRouter } from 'react-router-dom';
 
-ReactDOM.render((
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-), document.getElementById('root'));
+const rehydrateState = (window as any).ASYNC_COMPONENTS_STATE;
+const app = (
+  <AsyncComponentProvider rehydrateState={rehydrateState}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </AsyncComponentProvider>
+);
+
+asyncBootstrapper(app).then(() => {
+  ReactDOM.render(app, document.getElementById('root'));
+});
