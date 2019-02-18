7a22060216b1f02df349af4abc59bdf57a410887
Jason <me@jasonharrison.us>
2019-02-18 02:51:02 -0500

    Move WebpackShellPlugin into webpack.common.js

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,17 +1,7 @@
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
-const WebpackShellPlugin = require('webpack-shell-plugin-next');
 
 module.exports = merge(common, {
   mode: 'production', 
-  plugins: [
-    new WebpackShellPlugin({
-       onBuildEnd: {
-         scripts: ['node server-build/server.js'],
-         blocking: false,
-         parallel: true,
-       }
-    })
-  ]
 });
 
