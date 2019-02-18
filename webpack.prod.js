2816a2277bd446a866d677c7f3325305013f1286
Jason <me@jasonharrison.us>
2019-02-18 02:46:34 -0500

    Changes to Dockerfile/docker-compose for production

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,7 +1,17 @@
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
+const WebpackShellPlugin = require('webpack-shell-plugin-next');
 
 module.exports = merge(common, {
   mode: 'production', 
+  plugins: [
+    new WebpackShellPlugin({
+       onBuildEnd: {
+         scripts: ['node server-build/server.js'],
+         blocking: false,
+         parallel: true,
+       }
+    })
+  ]
 });
 
