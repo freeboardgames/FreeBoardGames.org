1b7b3e4d00e5ced99e117d407f599d8ebef2cef0
Jason <me@jasonharrison.us>
2019-02-21 00:06:33 -0500

    Move homepage links up

--- src/Home/Header.test.tsx
+++ src/Home/Header.test.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { MemoryRouter } from 'react-router';
 import Header from './Header';
 import { mount, shallow } from 'enzyme';
 import { expect } from 'chai';
@@ -6,7 +7,7 @@ import { expect } from 'chai';
 describe('Header', () => {
 
   it('should contain Chess option', () => {
-    const wrapper = mount(<Header />);
+    const wrapper = mount(<MemoryRouter><Header /></MemoryRouter>);
     expect(wrapper.text()).to.include('Free as in freedom');
   });
 
--- src/Home/Home.tsx
+++ src/Home/Home.tsx
@@ -11,20 +11,7 @@ class Home extends React.Component<{}, {}> {
         <Header />
         <GamesList />
         <p style={{ fontSize: '12px', textAlign: 'center' }}>
-          Made with ♥&nbsp;-&nbsp;
-          <a
-            href="https://github.com/freeboardgame/FreeBoardGame.org"
-            target="_blank"
-            rel="noopener"
-          >
-            GitHub
-          </a>
-          &nbsp;-&nbsp;
-          <Link
-            to="/about"
-          >
-            About
-          </Link>
+          Made with ♥
         </p>
       </FreeBoardGameBar>
     );
