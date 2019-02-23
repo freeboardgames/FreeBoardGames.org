5d7095549980fef127d1adcc04a02912f4369ab9
Jason <me@jasonharrison.us>
2019-02-22 23:29:05 -0500

    Add basic game.test.ts for tictactoe

---
+++ src/games/tictactoe/game.test.ts
@@ -0,0 +1,30 @@
+import { Client } from 'flamecoals-boardgame.io/client';
+import { TictactoeGame } from './game';
+
+// https://github.com/nicolodavis/boardgame.io/blob/master/docs/testing.md
+it('should declare player 1 as the winner', () => {
+  // set up a specific board scenario
+  const TictactoeCustomScenario = {
+    ...TictactoeGame,
+    setup: () => ({
+      cells: ['0', '0', null, '1', '1', null, null, null, null],
+    }),
+  };
+
+  // initialize the client with your custom scenario
+  const client = Client({
+    game: TictactoeCustomScenario,
+  });
+
+  // make some game moves
+  client.moves.clickCell(8);
+  client.moves.clickCell(5);
+
+  // get the latest game state
+  const { G, ctx } = client.store.getState();
+
+  // the board should look like this now
+  expect(G.cells).toEqual(['0', '0', null, '1', '1', '1', null, null, '0']);
+  // player '1' should be declared the winner
+  expect(ctx.gameover).toEqual({ winner: '1' });
+});
