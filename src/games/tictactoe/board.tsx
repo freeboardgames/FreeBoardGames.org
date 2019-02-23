5655eb12e1ea0a4ac1f1ddb6a3a81aee82a9e80d
Jason <me@jasonharrison.us>
2019-02-22 22:56:47 -0500

    TicTacToe: singleplayer and multiplayer working

--- src/games/tictactoe/board.tsx
+++ src/games/tictactoe/board.tsx
@@ -8,7 +8,9 @@
 
 import * as React from 'react';
 import * as PropTypes from 'prop-types';
+import { IGameArgs } from '../../App/Game/GameBoardWrapper';
 import { GameLayout } from '../../App/Game/GameLayout';
+import { GameMode } from '../../App/Game/GameModePicker';
 
 interface IBoardProps {
   G: any;
@@ -18,23 +20,33 @@ interface IBoardProps {
   isActive: boolean;
   isConnected: boolean;
   isMultiplayer: boolean;
-  isPreview: boolean
+  isPreview: boolean;
+  gameArgs?: IGameArgs;
 }
 
 interface IBoardState {
+  turn: string;
 }
 
 export class Board extends React.Component<IBoardProps, IBoardState> {
-  lineStyle = {
+  private lineStyle = {
     stroke: 'white',
     strokeWidth: .05,
   };
+
+  constructor(props: IBoardProps) {
+    super(props);
+    this.state = {
+      turn: 'X',
+    };
+  }
+
   onClick = (id: number) => () => {
     if (this.isActive(id)) {
+      this.takeTurn();
       this.props.moves.clickCell(id);
     }
   }
-
   makeCross(x: number, y: number) {
     const crosses = [
       <line key={`cross0${x}${y}`} x1={x} y1={y} x2={x + 1} y2={y + 1} style={this.lineStyle} />,
@@ -51,10 +63,70 @@ export class Board extends React.Component<IBoardProps, IBoardState> {
     return this.props.isActive && this.props.G.cells[id] === null;
   }
 
+  getPlayer(): 'X' | 'O' {
+    if (this.props.playerID === '0') {
+      return 'X';
+    } else {
+      return 'O';
+    }
+  }
+
+  isOnlineGame() {
+    return (this.props.gameArgs && this.props.gameArgs.mode === GameMode.OnlineFriend);
+  }
+
+  takeTurn() {
+    let turn;
+    if (this.state.turn === 'X') {
+      turn = 'O';
+    } else {
+      turn = 'X';
+    }
+    this.setState({ ... this.state, turn });
+    return turn;
+  }
+
+  _getStatus() {
+    if (this.isOnlineGame()) {
+      if (this.props.ctx.currentPlayer === this.props.playerID) {
+        return 'YOUR TURN';
+      } else {
+        return 'Waiting for opponent...';
+      }
+    } else { // Local game
+      switch (this.state.turn) {
+        case 'X': return 'X\'s turn';
+        case 'O': return 'O\'s turn';
+      }
+    }
+  }
+
+  _getGameOver() {
+    if (this.props.gameArgs && this.props.gameArgs.mode === GameMode.OnlineFriend) {
+      // Online game
+      if (this.props.ctx.gameover.winner !== undefined) {
+        if (this.props.ctx.gameover.winner !== this.props.playerID) {
+          return 'you won';
+        } else {
+          return 'you lost';
+        }
+      } else {
+        return 'draw';
+      }
+    } else {
+      // Local game
+      switch (this.props.ctx.gameover.winner) {
+        case '0': return 'X won';
+        case '1': return 'O won';
+        case undefined: return 'draw';
+      }
+    }
+  }
+
   render() {
     if (this.props.ctx.gameover) {
       return (
-        <GameLayout gameOver={this.props.ctx.gameover.winner} />
+        <GameLayout gameOver={this._getGameOver()} />
       );
     }
     const cellStyle = {
@@ -102,19 +174,9 @@ export class Board extends React.Component<IBoardProps, IBoardState> {
       disconnected = <div>Disconnected!</div>;
     }
 
-    let winner = null;
-    if (this.props.ctx.gameover) {
-      winner =
-        this.props.ctx.gameover.winner !== undefined ? (
-          <div id="winner">Winner: {this.props.ctx.gameover.winner}</div>
-        ) : (
-          <div id="winner">Draw!</div>
-        );
-    }
-
     let player = null;
     if (this.props.playerID) {
-      player = <div id="player">Player: {this.props.playerID}</div>;
+      player = <div id="player">Player: {this.getPlayer()}</div>;
     }
 
     if (this.props.isPreview) {
@@ -124,12 +186,14 @@ export class Board extends React.Component<IBoardProps, IBoardState> {
     return (
       <GameLayout>
         <div>
+          <h2 style={{ textAlign: 'center' }}>
+            {this._getStatus()}
+          </h2>
           <svg width="100%" height="100%" viewBox="0 0 3 3">
             {cells}
             {lines}
           </svg>
           {player}
-          {winner}
           {disconnected}
         </div>
       </GameLayout>
--- src/games/tictactoe/game.ts
+++ src/games/tictactoe/game.ts
@@ -9,7 +9,7 @@
 import { Game } from 'flamecoals-boardgame.io/core';
 
 export function isVictory(cells: number[]) {
-  const positions  = [
+  const positions = [
     [0, 1, 2],
     [3, 4, 5],
     [6, 7, 8],
