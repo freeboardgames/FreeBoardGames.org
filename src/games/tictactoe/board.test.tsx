f88417f0882c0db1de4432003814abfe11129e12
Jason <me@jasonharrison.us>
2019-02-23 22:29:04 -0500

    Add more tests for tictactoe

--- src/games/tictactoe/board.test.tsx
+++ src/games/tictactoe/board.test.tsx
@@ -18,7 +18,6 @@ test('render board - one X and one O - local friend', () => {
   });
   client.moves.clickCell(0); // X on the top left cell
   client.moves.clickCell(4); // O on the middle cell
-  client.moves.clickCell(8);  // X on the bottom right cell
   const state0 = client.store.getState();
   const comp = Enzyme.mount((
     <MemoryRouter>
@@ -31,6 +30,67 @@ test('render board - one X and one O - local friend', () => {
       />
     </MemoryRouter>
   ));
+  expect(comp.html()).to.contain('X\'s turn');
+  expect(comp.find('.cross').length).to.equal(1); // one X
   expect(comp.find('circle').length).to.equal(1); // one O
-  expect(comp.find('.cross').length).to.equal(2); // two X
+});
+
+test('render board - X wins - local friend', () => {
+  const client = Client({
+    game: TictactoeGame,
+  });
+  const state0 = client.store.getState();
+  const state1 = { ...state0, ctx: { ...state0.ctx, gameover: { winner: '0' } } };
+  const comp = Enzyme.mount((
+    <MemoryRouter>
+      <Board
+        G={state1.G}
+        ctx={state1.ctx}
+        moves={client.moves}
+        playerID={'0'}
+        isActive={true}
+      />
+    </MemoryRouter>
+  ));
+  expect(comp.html()).to.contain('X won');
+});
+
+test('render board - O wins - local friend', () => {
+  const client = Client({
+    game: TictactoeGame,
+  });
+  const state0 = client.store.getState();
+  const state1 = { ...state0, ctx: { ...state0.ctx, gameover: { winner: '1' } } };
+  const comp = Enzyme.mount((
+    <MemoryRouter>
+      <Board
+        G={state1.G}
+        ctx={state1.ctx}
+        moves={client.moves}
+        playerID={'0'}
+        isActive={true}
+      />
+    </MemoryRouter>
+  ));
+  expect(comp.html()).to.contain('O won');
+});
+
+test('render board - draw - local friend', () => {
+  const client = Client({
+    game: TictactoeGame,
+  });
+  const state0 = client.store.getState();
+  const state1 = { ...state0, ctx: { ...state0.ctx, gameover: { winner: undefined } } };
+  const comp = Enzyme.mount((
+    <MemoryRouter>
+      <Board
+        G={state1.G}
+        ctx={state1.ctx}
+        moves={client.moves}
+        playerID={'0'}
+        isActive={true}
+      />
+    </MemoryRouter>
+  ));
+  expect(comp.html()).to.contain('draw');
 });
