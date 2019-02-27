77d943e1e42d34d6b463452b11f6285d5166296d
Jason <me@jasonharrison.us>
2019-02-26 21:27:26 -0500

    Add more tests for tictactoe

--- src/games/tictactoe/game.test.ts
+++ src/games/tictactoe/game.test.ts
@@ -28,3 +28,25 @@ it('should declare player 1 as the winner', () => {
   // player '1' should be declared the winner
   expect(ctx.gameover).toEqual({ winner: '1' });
 });
+
+it('should declare a draw', () => {
+  // set up a specific board scenario
+  const TictactoeCustomScenario = {
+    ...TictactoeGame,
+    setup: () => ({
+      cells: ['1', '0', '1', '1', '0', '0', '0', '1', null],
+    }),
+  };
+
+  // initialize the client with your custom scenario
+  const client = Client({
+    game: TictactoeCustomScenario,
+  });
+
+  client.moves.clickCell(8);
+  // get the latest game state
+  const { G, ctx } = client.store.getState();
+
+  // a draw should be declared
+  expect(ctx.gameover).toEqual({ draw: true });
+});
