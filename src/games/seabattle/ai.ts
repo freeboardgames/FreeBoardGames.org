45520e367c9355d9ed76decacdf8f3367a30d18c
flamecoals <flamecoals@users.noreply.github.com>
2019-03-09 22:09:05 -0800

    random ai works

--- src/games/seabattle/ai.ts
+++ src/games/seabattle/ai.ts
@@ -1,4 +1,5 @@
 import { IAIConfig } from '../index';
+import { IShip, generateRandomShips } from './game';
 
 interface IPlayState {
   G: any;
@@ -7,7 +8,19 @@ interface IPlayState {
 
 class SeabattleBot {
   async play(state: IPlayState, playerID: string) {
-    return {};
+    if (state.ctx.phase === 'setup') {
+      return this.makeSetShipsMove(generateRandomShips(1));
+    } else {
+      return this.makeSalvoMove(Math.floor(Math.random() * 10), Math.floor(Math.random() * 10));
+    }
+  }
+
+  makeSetShipsMove(ships: IShip[]) {
+    return { action: { type: 'MAKE_MOVE', payload: { type: 'setShips', args: [ships], playerID: '1' } } };
+  }
+
+  makeSalvoMove(x: number, y: number) {
+    return { action: { type: 'MAKE_MOVE', payload: { type: 'salvo', args: [x, y], playerID: '1' } } };
   }
 }
 
