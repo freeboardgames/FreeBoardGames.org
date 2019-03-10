45f469b8dd5c07f5b3c4e3ef17d82880d6759f33
Jason <me@jasonharrison.us>
2019-03-10 00:35:17 -0500

    Continue work on Seabattle AI

--- src/games/seabattle/ai.ts
+++ src/games/seabattle/ai.ts
@@ -5,29 +5,9 @@ interface IPlayState {
   ctx: any;
 }
 
-class SeabattleAI {
-  worker: any;
-  level: number;
-
-  getMove(fen: string): any {
-    return null;
-  }
-}
-
-const seabattleAI = new SeabattleAI();
-
 class SeabattleBot {
   async play(state: IPlayState, playerID: string) {
-    if (!state.ctx.gameover) {
-      const move = await seabattleAI.getMove(state.G.fen);
-      return this.makeMove(move, state.ctx.currentPlayer);
-    } else {
-      return {};
-    }
-  }
-
-  makeMove(move: string, playerID: string) {
-    return { action: { type: 'MAKE_MOVE', payload: { type: 'move', args: [move], playerID } } };
+    return {};
   }
 }
 
