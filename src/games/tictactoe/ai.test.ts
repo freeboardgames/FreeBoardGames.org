c2be8ec71b8cfaf12e98414bf7f737675437b326
Jason <me@jasonharrison.us>
2019-03-10 20:47:12 -0400

    Fix AI test

--- src/games/tictactoe/ai.test.ts
+++ src/games/tictactoe/ai.test.ts
@@ -3,7 +3,21 @@ import { expect } from 'chai';
 import { TictactoeGame } from './game';
 import { Client } from '@freeboardgame.org/boardgame.io/client';
 
-test('Hard - should return a move for the initial position', async () => {
+test('Easy AI - should return a move for the initial position', async () => {
+  const client = Client({
+    game: TictactoeGame,
+    ai: AIConfig.bgioAI('Easy'),
+  });
+  client.moves.clickCell(0);  // player plays
+  await client.step();  // AI plays
+  const { G, ctx } = client.store.getState();
+  const player0Moves = G.cells.filter((cell: string) => cell === '0');
+  const player1Moves = G.cells.filter((cell: string) => cell === '1');
+  expect(player0Moves.length).to.equal(1);
+  expect(player1Moves.length).to.equal(1);
+});
+
+test('Hard AI - should return a move for the initial position', async () => {
   const client = Client({
     game: TictactoeGame,
     ai: AIConfig.bgioAI('Hard'),
