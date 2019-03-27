f9bc33ad85ef4d4536eaec176816ec304c01f657
Jason <me@jasonharrison.us>
2019-03-27 18:39:37 -0400

    seabattle AI is working!

--- src/games/seabattle/ai.ts
+++ src/games/seabattle/ai.ts
@@ -8,22 +8,15 @@ interface IAISalvo extends ISalvo {
   right: boolean;
 }
 
-// interface IAISalvoTarget {
-//     cells: ICell[];
-// }
-
 interface IPlayState {
   G: any;
   ctx: any;
-//   AISalvoTargets: IAISalvoTarget[];
-//   AISalvos: IAISalvo[];
 }
 
 class SeabattleBot {
   async play(state: IPlayState, playerID: string) {
     if (state.ctx.phase === 'setup') {
       const shipPositions = generateRandomShips(1);
-      console.log(shipPositions);
       return this.makeSetShipsMove(shipPositions);
     } else {
       const cell = this.generateMove(state);
@@ -40,71 +33,96 @@ class SeabattleBot {
   }
 
   generateMove(state: IPlayState) {
+    if (state.G.salvos.length === 0) {
+      const move: ICell = { x: 0, y: 0 };
+      return move;
+    }
     const salvos = state.G.salvos.filter((salvo: ISalvo) => (
       salvo.player === 1 &&
       salvo.hit === true &&
       typeof salvo.hitShip !== 'undefined'));
     for (const salvo of salvos) {
-      if (this.isShipSunk(state, salvo.hitShip)) {
-        console.log('Ship has sunk ' + salvo.hitShip);
-      } else {
-        console.log('Ship has not sunk ' + salvo.hitShip);
+      // if we have already fired salvos, see if we've hit any ships that remain unsunk
+      if (!this.isShipSunk(state, salvo.hitShip)) {
         const otherHitSalvos = this.getOtherSalvosHitShip(state, salvo.hitShip);
-        if (otherHitSalvos.length === 0) {
-          // no other salvos for the same ship were hit, so hit a random neighbor
-          return this.getRandomNeighbor(state, salvo);
+        if (otherHitSalvos.length >= 2) {
+          // we have hit this ship at least twice, so we can make an intelligent move
+          return this.getMoveFromOtherSalvos(state, otherHitSalvos);
         }
-        return this.getNextMoveFromNeighbors(state, otherHitSalvos);
+        // no other salvos for the same ship were hit, so hit a random neighbor
+        return this.getRandomNeighbor(state, salvo);
       }
     }
+    // generate a random move
     return this.generateRandomMove(state);
   }
 
-  getNextMoveFromNeighbors(state: IPlayState, salvos: ISalvo[]): ICell {
-    const len = salvos.length;
-    const lastHitSalvo = salvos[len - 1];
-    if (salvos.length > 1) {
-      const hitCell1 = lastHitSalvo.cell;
-      const hitCell0 = salvos[len - 2].cell;  // second to last hit
-      const move = hitCell1;  // move to be made
-      if (hitCell1.x === hitCell0.x) {
-        // ship is vertical, so move horizontally
-        move.y = hitCell1.y - hitCell0.y;
-        if (!this.isValidMove(state, move)) {
-          move.y = hitCell1.y + hitCell0.y;  // undo last move
-          move.y = hitCell1.y + hitCell0.y;  // try other way
-        }
-      } else {
-        // ship is horizontal
-        move.x = hitCell1.x - hitCell0.x;
-        if (!this.isValidMove(state, move)) {
-          move.x = hitCell1.x + hitCell0.x;
-          move.x = hitCell1.x + hitCell0.x;
-        }
+  getMoveFromOtherSalvos(state: IPlayState, hitSalvos: ISalvo[]): ICell {
+    const len = hitSalvos.length;
+    const lastHitSalvo = hitSalvos[len - 1];
+    const lastHitCell = lastHitSalvo.cell;
+    const secondLastHitCell = hitSalvos[len - 2].cell;  // second to last hit
+    const firstHitCell = hitSalvos[0].cell;
+
+    let move: ICell = { x: lastHitCell.x, y: lastHitCell.y };  // set to last hit cell
+    let moveDelta;
+    if (lastHitCell.x === secondLastHitCell.x) {
+      // ship is vertical x, so move horizontal y
+      moveDelta = lastHitCell.y - secondLastHitCell.y;
+      if (moveDelta !== -1 && moveDelta !== 1) {
+        return this.getRandomNeighbor(state, lastHitSalvo);
       }
-      return move;
+      if (this.isInBounds(move.y + moveDelta)) {
+        move = { x: move.x, y: move.y + moveDelta };  // reset to first hit cell
+      } else {
+        move = { x: firstHitCell.x, y: firstHitCell.y - moveDelta };  // reset to first hit cell
+      }
+    } else if (lastHitCell.y === secondLastHitCell.y) {
+      // ship is horizontal y, so move vertical x
+      moveDelta = lastHitCell.x - secondLastHitCell.x;
+      if (moveDelta !== -1 && moveDelta !== 1) {
+        return this.getRandomNeighbor(state, lastHitSalvo);
+      }
+      if (this.isInBounds(move.x + moveDelta)) {
+        move = { x: move.x + moveDelta, y: move.y };  // reset to first hit cell
+      } else {
+        move = { x: firstHitCell.x - moveDelta, y: firstHitCell.y };  // reset to first hit cell
+      }
     } else {
+      // we hit a different ship than the one before, so hit a random neighbor:
       return this.getRandomNeighbor(state, lastHitSalvo);
     }
+    return move;
+  }
+
+  isInBounds(x: number) {
+    const valid = (x >= 0 && x <= 9);
+    return valid;
   }
 
   isValidMove(state: IPlayState, cell: ICell) {
-    return (cell.x >= 0 && cell.x <= 10 &&
-           cell.y >= 0 && cell.y <= 10 &&
-           this.isUniqueMove(state, cell));
+    const valid = (this.isInBounds(cell.x) && this.isInBounds(cell.y) && this.isUniqueMove(state, cell));
+    return valid;
+  }
+
+  areVerticalMovesPossible(state: IPlayState, move: ICell) {
+    const possible = (this.isValidMove(state, move));
   }
 
   getRandomNeighbor(state: IPlayState, salvo: ISalvo): ICell {
     let i = 0;
-    let move;
-    do {
-      move = salvo.cell;
-      const vertical = this.genRandomNumber(0, 1) === 0;
-      const randomChange = (this.genRandomNumber(0, 1) === 0) ? -1 : 1;
+    let vertical;
+    let validMove;
+    let randomChange;
+    let move: ICell;
+    while (!validMove && i < 10) {
+      vertical = this.genRandomNumber(0, 1) === 0;
+      move = { x: salvo.cell.x, y: salvo.cell.y };
+      randomChange = (this.genRandomNumber(0, 1) === 0) ? -1 : 1;
       if (vertical) {
         if (move.x === 0) {
           move.x++;
-        } else if (move.x === 10) {
+        } else if (move.x === 9) {
           move.x--;
         } else {
           move.x += randomChange;
@@ -112,15 +130,15 @@ class SeabattleBot {
       } else {
         if (move.y === 0) {
           move.y++;
-        } else if (move.y === 10) {
+        } else if (move.y === 9) {
           move.y--;
         } else {
           move.y += randomChange;
         }
       }
-      console.log(`Hitting random neighbor, ${move.x} ${move.y}`);
+      validMove = this.isValidMove(state, move);
       i++;
-    } while (i < 1000 && (!this.isValidMove(state, move)));
+    }
     return move;
   }
 
@@ -138,8 +156,8 @@ class SeabattleBot {
 
   generateRandomMove(state: IPlayState) {
     while (true) {
-      const x = Math.floor(Math.random() * 10);
-      const y = Math.floor(Math.random() * 10);
+      const x = this.genRandomNumber(0, 9);
+      const y = this.genRandomNumber(0, 9);
       const randomCell: ICell = { x, y };
       const unique = this.isUniqueMove(state, randomCell);
       if (unique) {
@@ -148,16 +166,17 @@ class SeabattleBot {
     }
   }
 
-  getAISalvos(state: IPlayState, cell: ICell) {
-    return state.G.salvos.filter((salvo: ISalvo) => (
+  getAISalvos(state: IPlayState) {
+    const salvos = state.G.salvos.filter((salvo: ISalvo) => (
       salvo.player === 1));
+    return salvos;
   }
 
   isUniqueMove(state: IPlayState, cell: ICell) {
-    const uniqueMove = state.G.salvos.filter((salvo: ISalvo) => (
-      salvo.player === 1 &&
-      salvo.cell.x === cell.x &&
-      salvo.cell.y === cell.y)).length === 0;
+    const moves = state.G.salvos.filter((salvo: ISalvo) => (
+      salvo.player === 1 && salvo.cell.x === cell.x && salvo.cell.y === cell.y),
+    );
+    const uniqueMove = moves.length === 0;
     return uniqueMove;
   }
 
