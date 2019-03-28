a603ac181da94c213374341ed1e7cf2f27cdcac6
Jason <me@jasonharrison.us>
2019-03-27 21:34:16 -0400

    Add AI tests

---
+++ src/games/seabattle/ai.test.ts
@@ -0,0 +1,37 @@
+import AIConfig from './ai';
+import { Client } from '@freeboardgame.org/boardgame.io/client';
+import { SeabattleGame, IShip, generateRandomShips, validateShips, playerView } from './game';
+import { VALID_SETUP_FIRST_PLAYER, VALID_SETUP_SECOND_PLAYER } from './mocks';
+
+it('should set ships correctly', async () => {
+  const client = genClient();
+
+  client.moves.setShips(VALID_SETUP_FIRST_PLAYER);
+  client.updatePlayerID('1');
+
+  // AI setShips
+  await client.step();
+
+  expect(client.store.getState().ctx.phase).toEqual('play');
+});
+
+it('should shoot a random salvo', async () => {
+  const client = genClient();
+
+  client.moves.setShips(VALID_SETUP_FIRST_PLAYER);
+  client.updatePlayerID('1');
+  client.moves.setShips(VALID_SETUP_SECOND_PLAYER);
+
+  // AI shoots random salvo
+  await client.step();
+
+  const state = client.store.getState();
+  expect(state.G.salvos.length).toEqual(1);
+});
+
+function genClient() {
+  return Client({
+    game: SeabattleGame,
+    ai: AIConfig.bgioAI('1'),
+  });
+}
