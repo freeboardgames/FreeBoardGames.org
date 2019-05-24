8d98eb9e85fb0b63a0594b97e769ca85f30693ff
Jason <me@jasonharrison.us>
2019-05-23 21:38:29 -0400

    AddressHelper: Always specify protocol

--- src/App/AddressHelper.ts
+++ src/App/AddressHelper.ts
@@ -2,7 +2,7 @@ export default class AddressHelper {
   /** Gets bgio host:port address. */
   public static getServerAddress() {
     if (typeof window !== 'undefined') {
-      return process.env.BGIO_SERVER_URL || `${window.location.hostname}:8001`;
+      return process.env.BGIO_SERVER_URL || `http://${window.location.hostname}:8001`;
     }
   }
 }
--- src/App/Lobby/LobbyService.ts
+++ src/App/Lobby/LobbyService.ts
@@ -4,7 +4,7 @@ import request from 'superagent';
 export class LobbyService {
   public static async newRoom(gameCode: string): Promise<string> {
     const response = await request
-      .post(`http://${AddressHelper.getServerAddress()}/games/${gameCode}/create`)
+      .post(`${AddressHelper.getServerAddress()}/games/${gameCode}/create`)
       .send({ numPlayers: 2 });
     // TODO make user join automatically this room, store credentials
     return response.body.gameID;
