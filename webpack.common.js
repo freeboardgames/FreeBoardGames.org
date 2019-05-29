de4bb0cdf4a075f097a9ec05f3b24946bbca3d7d
Jason <me@jasonharrison.us>
2019-05-29 01:58:02 +0000

    Split more chunks with webpack

--- webpack.common.js
+++ webpack.common.js
@@ -14,9 +14,26 @@ var config = {
   },
 
   optimization: {
+    runtimeChunk: 'single',
+    // https://hackernoon.com/the-100-correct-way-to-split-your-chunks-with-webpack-f8a9df5b7758
     splitChunks: {
-      chunks: 'all'
-    }
+      chunks: 'all',
+      maxInitialRequests: Infinity,
+      minSize: 0,
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name(module) {
+            // get the name. E.g. node_modules/packageName/not/this/part.js
+            // or node_modules/packageName
+            const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1];
+
+            // npm package names are URL-safe, but some servers don't like @ symbols
+            return `npm.${packageName.replace('@', '')}`;
+          },
+        },
+      },
+    },
   },
 
   output: {
