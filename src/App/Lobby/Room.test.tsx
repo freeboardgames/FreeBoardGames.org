af0862ae14824bf7a7a51b90e282bdeae28d40c9
Jason <me@jasonharrison.us>
2019-05-30 18:14:22 -0400

    Begin work on Room.test.tsx

---
+++ src/App/Lobby/Room.test.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Room } from './Room';
+import { mount, shallow } from 'enzyme';
+import { expect } from 'chai';
+import { IPlayerInRoom } from './LobbyService';
+import { MemoryRouter } from 'react-router';
+
+const player1: IPlayerInRoom = { playerID: 0, name: 'foobar', roomID: 'barroom' };
+
+describe('Room Lobby', () => {
+  it('should prompt for name', () => {
+    const app = (
+      <MemoryRouter>
+        <Room
+          match={{
+            params: { gameCode: 'foogame', roomID: 'fooroom' },
+          }}
+        />
+      </MemoryRouter>
+    );
+    const wrapper = mount(app);
+    expect(wrapper.html()).to.contain('Enter Your Nickname');
+  });
+});
