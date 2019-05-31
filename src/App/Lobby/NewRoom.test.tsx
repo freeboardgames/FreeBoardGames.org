60d0aac17a237e60d759d61447bd5a413fc47f84
Jason <me@jasonharrison.us>
2019-05-31 02:21:36 -0400

    Add NewRoom.test.tsx

---
+++ src/App/Lobby/NewRoom.test.tsx
@@ -0,0 +1,61 @@
+import React from 'react';
+import { mount, shallow } from 'enzyme';
+import { expect } from 'chai';
+import { IPlayerInRoom } from './LobbyService';
+import { MemoryRouter } from 'react-router';
+import { NewRoom } from './NewRoom';
+import { LobbyService } from './LobbyService';
+
+describe('New Room', () => {
+  it('should redirect', async () => {
+    const p = Promise.resolve('roomfoo');
+    LobbyService.newRoom = jest.fn().mockReturnValue(p);
+    const historyMock = jest.fn();
+    const app = (
+      <MemoryRouter>
+        <NewRoom
+          match={{
+            params: { gameCode: 'chess' },
+          }}
+          history={{
+            push: historyMock,
+          }}
+        />
+      </MemoryRouter>
+    );
+    mount(app);
+    await p;
+    expect(historyMock.mock.calls.length).to.equal(1);
+  });
+
+  it('should show error if given invalid gamecode', () => {
+    const app = (
+      <MemoryRouter>
+        <NewRoom
+          match={{
+            params: { gameCode: 'notagame' },
+          }}
+        />
+      </MemoryRouter>
+    );
+    const wrapper = mount(app);
+    expect(wrapper.html()).contains('Failed to create room');
+  });
+
+  it('should show error if promise does not resolve', async () => {
+    const p = Promise.reject();
+    LobbyService.newRoom = jest.fn().mockReturnValue(p);
+    const app = (
+      <MemoryRouter>
+        <NewRoom
+          match={{
+            params: { gameCode: 'chess' },
+          }}
+        />
+      </MemoryRouter>
+    );
+    const wrapper = mount(app);
+    await wrapper;
+    expect(wrapper.html()).contains('Failed to create room');
+  });
+});
