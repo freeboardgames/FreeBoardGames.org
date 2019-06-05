71ef270edc97a13b21f07dfec7e41d63566ad863
Jason <me@jasonharrison.us>
2019-06-05 01:07:33 -0400

    Remove line break

--- src/App/translations/en.ts
+++ src/App/translations/en.ts
@@ -2,8 +2,7 @@
 const en = {
   about: {
     header: 'About FreeBoardGame.org',
-    p:
-      'FreeBoardGame.org is a free (as in freedom), mobile-first, board game platform. Its goal is to popularize board games and to make them easy to play with friends, even from afar.',
+    p: 'FreeBoardGame.org is a free (as in freedom), mobile-first, board game platform. Its goal is to popularize board games and to make them easy to play with friends, even from afar.',
     credits: 'Credits',
     contributors: 'Contributors',
   },
