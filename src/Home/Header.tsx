3302f8467708328a9891d507718c8446d6c162e4
flamecoals <flamecoals@users.noreply.github.com>
2019-06-22 19:09:19 -0700

    Adding blog link to home page

--- src/Home/Header.tsx
+++ src/Home/Header.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Button from '@material-ui/core/Button';
 import CodeIcon from '@material-ui/icons/Code';
 import QuestionAnswerIcon from '@material-ui/icons/QuestionAnswer';
+import SubjectIcon from '@material-ui/icons/Subject';
 import Typography from '@material-ui/core/Typography';
 
 class Header extends React.Component<{}, {}> {
@@ -11,7 +12,7 @@ class Header extends React.Component<{}, {}> {
         <Typography variant="h6" gutterBottom={true} align="center" style={{ marginTop: '16px' }}>
           Free as in freedom
         </Typography>
-        <Typography variant="body1" gutterBottom={true} style={{ marginTop: '16px' }}>
+        <Typography variant="body2" gutterBottom={true} style={{ marginTop: '16px' }}>
           We at FreeBoardGame.org want to bring free games for everybody. Free as in &quot;free beer&quot; <i>and</i> as
           in &quot;freedom&quot;. Not only do you get to enjoy free high quality games everywhere, but you also can
           study how they are made, change them, and contribute back to the community!
@@ -22,19 +23,23 @@ class Header extends React.Component<{}, {}> {
   }
   _links() {
     return (
-      <div style={{ textAlign: 'center', marginTop: '8px' }}>
+      <div style={{ textAlign: 'center', marginTop: '4px' }}>
         <Button
           href="https://github.com/freeboardgame/FreeBoardGame.org"
           target="_blank"
           variant="outlined"
           style={{ margin: '8px' }}
         >
-          <CodeIcon style={{ marginRight: '8px' }} />
-          GitHub
+          <CodeIcon style={{ marginRight: '4px' }} />
+          Code
         </Button>
-        <Button href="https://discord.gg/AaE6n3n" target="_blank" variant="outlined" style={{ margin: '8px' }}>
-          <QuestionAnswerIcon style={{ marginRight: '8px' }} />
-          Discord
+        <Button href="https://freeboardgame.org/blog" target="_blank" variant="outlined" style={{ margin: '4px' }}>
+          <SubjectIcon style={{ marginRight: '4px' }} />
+          Blog
+        </Button>
+        <Button href="https://discord.gg/AaE6n3n" target="_blank" variant="outlined" style={{ margin: '4px' }}>
+          <QuestionAnswerIcon style={{ marginRight: '4px' }} />
+          Chat
         </Button>
       </div>
     );
