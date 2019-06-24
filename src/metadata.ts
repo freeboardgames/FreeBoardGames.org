5fa7bfdbf05ac4eab5c451aea1ed92ae74b56298
Jason <me@jasonharrison.us>
2019-06-24 17:38:06 -0400

    Add breadcrumbs support for SEO: https://developers.google.com/search/docs/data-types/breadcrumb

--- src/metadata.ts
+++ src/metadata.ts
@@ -1,13 +1,16 @@
 import { GAMES_LIST } from './games';
 
 export interface IPageMetadata {
+  name?: string;
   title: string;
   description?: string;
   url?: RegExp;
+  link?: string;
   noindex?: boolean;
 }
 
 const TITLE_PREFIX = 'FreeBoardGame.org - ';
+const URL_BASE = 'https://FreeBoardGame.org';
 
 const DEFAULT_METADATA: IPageMetadata = {
   title: 'FreeBoardGame.org',
@@ -19,9 +22,11 @@ Compete against your online friends or play locally. Free and open-source softwa
 // Most specific URLs MUST come first.
 const PAGES_METADATA: IPageMetadata[] = [
   {
+    name: 'About Us',
     title: TITLE_PREFIX + 'About Us',
     description: 'About FreeBoardGame.org, a free and open-source software project.',
     url: new RegExp('^/about', 'i'),
+    link: URL_BASE + '/about',
   },
   {
     title: TITLE_PREFIX + 'Play Free Board Games Online',
@@ -33,9 +38,11 @@ Compete against your online friends or play locally. Free and open-source softwa
 
 function getGamesPageMetadata(): IPageMetadata[] {
   return GAMES_LIST.map(gameDef => ({
+    name: gameDef.name,
     title: TITLE_PREFIX + `Play Free ${gameDef.name} Online`,
     description: gameDef.descriptionTag,
     url: new RegExp(`^/g/${gameDef.code}$`, 'i'),
+    link: `${URL_BASE}/g/${gameDef.code}`,
   }));
 }
 
@@ -47,4 +54,26 @@ export const getPageMetadata = (url: string): IPageMetadata => {
     return DEFAULT_METADATA;
   }
   return metadata;
+};
+
+export const getBreadcrumbs = (): string => {
+  const allPagesMetadata = [...PAGES_METADATA, ...getGamesPageMetadata()];
+  const pageElements = allPagesMetadata
+    .filter((pageMetadata: IPageMetadata) => {
+      return pageMetadata.name && pageMetadata.link; // check if we have both .name and .link
+    })
+    .map((pageMetadata: IPageMetadata, index: number) => {
+      return {
+        '@type': 'ListItem',
+        position: index + 1,
+        name: pageMetadata.name,
+        item: pageMetadata.link,
+      };
+    });
+  const breadcrumbList = {
+    '@context': 'https://schema.org',
+    '@type': 'BreadcrumbList',
+    itemListElement: pageElements,
+  };
+  return JSON.stringify(breadcrumbList);
 };
