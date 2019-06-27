8ab1d445c5dc5d6114035cce659f226594fcfa16
Jason <me@jasonharrison.us>
2019-06-26 23:59:27 -0400

    run eslint for javascript in test/ too

---
+++ .eslintrc-javascript.js
@@ -0,0 +1,16 @@
+module.exports = {
+  extends: [
+    'plugin:prettier/recommended', // Enables eslint-plugin-prettier and displays prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
+  ],
+  parserOptions: {
+    ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
+    sourceType: 'module', // Allows for the use of imports
+    ecmaFeatures: {
+      jsx: true, // Allows for the parsing of JSX
+    },
+  },
+  rules: {
+    'no-unused-vars': 'warn',
+    '@typescript-eslint/no-unused-vars': 'off',
+  },
+};
--- test/stockfish8Mock.js
+++ test/stockfish8Mock.js
@@ -1,10 +1,10 @@
-var StockfishMock = function () { };
+var StockfishMock = function() {};
 
-StockfishMock.prototype.postMessage = function (cmd) {
+StockfishMock.prototype.postMessage = function(cmd) {
   if (cmd.includes('go depth')) {
-    this.onmessage({data: 'not relevant message'});
-    this.onmessage({data: 'bestmove e2e3'});
+    this.onmessage({ data: 'not relevant message' });
+    this.onmessage({ data: 'bestmove e2e3' });
   }
-}
+};
 
 module.exports = StockfishMock;
