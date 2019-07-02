e4028ba1394bb55f4e3c09e13d63cabe75b075b6
Jason <me@jasonharrison.us>
2019-07-02 00:07:00 -0400

    Add WIP tests for GameDarkSublayout

--- src/App/Game/GameDarkSublayout.test.tsx
+++ src/App/Game/GameDarkSublayout.test.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
-import { GameDarkSublayout } from './GameDarkSublayout';
-import { mount } from 'enzyme';
+import { GameDarkSublayout, IOptionsItems } from './GameDarkSublayout';
+import { mount, ReactWrapper } from 'enzyme';
 import { expect } from 'chai';
 import { MemoryRouter } from 'react-router';
+import MoreVert from '@material-ui/icons/MoreVert';
 
 describe('Game Dark Sublayout', () => {
   it('should display', () => {
@@ -15,4 +16,27 @@ describe('Game Dark Sublayout', () => {
     );
     expect(wrapper.text()).to.contain('Foobar Game');
   });
+});
+
+describe('Game Dark Sublayout - with options menu', () => {
+  let onClickMock: jest.Mock;
+  let options: () => IOptionsItems[];
+  let wrapper: ReactWrapper;
+  beforeEach(() => {
+    onClickMock = jest.fn();
+    options = () => [{ text: 'Toggle me', onClick: onClickMock }];
+    wrapper = mount(
+      <MemoryRouter>
+        <GameDarkSublayout optionsMenuItems={options}>
+          <p>Foobar Game</p>
+        </GameDarkSublayout>
+      </MemoryRouter>,
+    );
+  });
+  it('should display morevert button', () => {
+    expect(wrapper.find(MoreVert).length).to.be.greaterThan(0);
+  });
+  // it('should show options menu', () => {
+  // TODO ...
+  // });
 });
