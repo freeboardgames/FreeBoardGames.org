dd17759fc6cd1a7424f96b6897ba51e587af0e4f
Jason <me@jasonharrison.us>
2020-01-11 22:54:01 -0500

    Connect 4 -> Four in a Row

--- games/fourinarow/index.ts
+++ games/fourinarow/index.ts
@@ -5,13 +5,13 @@ import instructions from './instructions.md';
 
 export const fourinarowGameDef: IGameDef = {
   code: 'fourinarow',
-  name: 'Connect 4',
+  name: 'Four in a Row',
   minPlayers: 2,
   maxPlayers: 2,
   imageURL: FourInARowThumbnail,
   modes: [{ mode: GameMode.OnlineFriend }, { mode: GameMode.LocalFriend }],
-  description: 'Also Known as Four-in-a-Row',
-  descriptionTag: `Connect Four is a two-player connection game in which the players first choose a color and then take turns dropping one colored disc from the top into a seven-column, six-row vertically suspended grid. The pieces fall straight down, occupying the lowest available space within the column. The objective of the game is to be the first to form a horizontal, vertical, or diagonal line of four of one's own discs.`,
+  description: 'Also Known as Connect Four',
+  descriptionTag: `Four in a Row is a two-player connection game in which the players first choose a color and then take turns dropping one colored disc from the top into a seven-column, six-row vertically suspended grid. The pieces fall straight down, occupying the lowest available space within the column. The objective of the game is to be the first to form a horizontal, vertical, or diagonal line of four of one's own discs.`,
   instructions: {
     videoId: 'utXzIFEVPjA',
     text: instructions,
