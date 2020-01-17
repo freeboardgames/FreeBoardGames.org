93f134ce866920b4cf81c982782004658a2c0778
Jason <me@jasonharrison.us>
2020-01-17 16:54:53 -0500

    Generate sitemap.xml

--- pages/play/index.tsx
+++ pages/play/index.tsx
@@ -1,3 +1,17 @@
-import RedirectToRoot from './RedirectToRoot';
+import React from 'react';
+import Router from 'next/router';
 
-export default RedirectToRoot;
+export default class extends React.Component {
+  static async getInitialProps({ res }) {
+    const redirectTo = '/';
+    if (res) {
+      res.writeHead(302, {
+        Location: redirectTo,
+      });
+      res.end();
+    } else {
+      Router.push(redirectTo);
+    }
+    return {};
+  }
+}
