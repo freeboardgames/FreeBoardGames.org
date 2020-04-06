6009e1b066b1a05084e2b188284f99e3ecf59727
flamecoals <1034631+flamecoals@users.noreply.github.com>
2020-04-05 19:02:37 -0700

    Fixes #463 (#465)
    Co-authored-by: flamecoals <flamecoals@users.noreply.github.com>

--- games/takesix/player.ts
+++ games/takesix/player.ts
@@ -1,13 +1,7 @@
 import Card from './card';
 
-export default class Player {
+export default interface Player {
   cards: Card[];
-  selectedCard: Card;
+  selectedCard?: Card;
   penaltyCards: Card[];
-
-  constructor(cards: Card[], selectedCard: Card) {
-    this.cards = cards;
-    this.selectedCard = selectedCard;
-    this.penaltyCards = [];
-  }
 }
