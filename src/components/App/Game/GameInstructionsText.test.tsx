7ac072527f2f03bfb0cc2822ff2c25881e2b8a5c
Jason Harrison <me@jasonharrison.us>
2020-04-10 04:25:24 -0400

    Improve test coverage for GameInstructionsText.test.tsx (#489)

--- src/components/App/Game/GameInstructionsText.test.tsx
+++ src/components/App/Game/GameInstructionsText.test.tsx
@@ -1,36 +1,32 @@
 import React from 'react';
+import Enzyme from 'enzyme';
 import { GameInstructionsText } from './GameInstructionsText';
-import { render, cleanup } from '@testing-library/react';
 import { IGameDef } from 'games';
 import { GameMode } from './GameModePicker';
 
-afterEach(cleanup);
-
-const gameDefNoText: IGameDef = {
+const gameDef: IGameDef = {
   code: 'foogame',
-  instructions: { videoId: 'videoId' },
   description: 'foodescription',
   descriptionTag: 'foodescriptiontag',
   name: 'foogame',
-  imageURL: 'fooimage',
+  imageURL: { src: 'fooimage', 0: 'fooimage' },
   minPlayers: 2,
   maxPlayers: 2,
   config: jest.fn(),
   modes: [{ mode: GameMode.LocalFriend }],
 };
 
-const gameDefWithText: IGameDef = {
-  ...gameDefNoText,
-  instructions: { ...gameDefNoText.instructions, text: 'foo game instructions' },
-};
+let comp: Enzyme.ReactWrapper;
 
-describe('Game Instructions', () => {
-  it('should not contain text instructions', () => {
-    const wrapper = render(<GameInstructionsText gameDef={gameDefNoText} />);
-    expect(wrapper.queryByText('foo game instructions')).not.toBeInTheDocument();
-  });
-  it('should contain text instructions', () => {
-    const wrapper = render(<GameInstructionsText gameDef={gameDefWithText} />);
-    expect(wrapper.queryByText('foo game instructions')).toBeInTheDocument();
-  });
+beforeEach(() => {
+  comp = Enzyme.mount(<GameInstructionsText gameDef={gameDef} />);
+});
+
+it('should not contain instructions', () => {
+  expect(comp.text()).toBeFalsy();
+});
+
+it('should contain instructions', () => {
+  comp.setProps({ gameDef: { ...gameDef, instructions: { text: 'foo' } } });
+  expect(comp.text()).toEqual('foo');
 });
