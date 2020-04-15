8c038fe3536597558185fe55793c0fde1789efe6
y s <ykse999@gmail.com>
2020-04-15 13:41:08 +0200

    adding more media

--- src/games/zooparade/components/btoken.tsx
+++ src/games/zooparade/components/btoken.tsx
@@ -9,10 +9,27 @@ interface InnerWrapper {
 
 export class BToken extends React.Component< InnerWrapper, {}> {
     render() {
+
+        var imagetreat = require('./media/treat.png');
+        var imagecountdown : any 
+
+        if (this.props.countdown === 3) {
+            imagecountdown = require('./media/countdown3.png');
+        } else if (this.props.countdown === 2) {
+            imagecountdown = require('./media/countdown2.png');
+        } else if (this.props.countdown === 1) {
+            imagecountdown = require('./media/countdown1.png');
+        }
+        
         return (
             <div>
-                <h2> Countdown : { this.props.countdown } </h2>
-                <h2> Treats : { this.props.treats } </h2>
+                    <div className="image">
+                        <img src={ imagetreat } height="100" width="100"></img>
+                        <h2> Treats : { this.props.treats } </h2>
+                    </div>
+                    <div className="image">
+                        <img src={ imagecountdown } height="100" width="100"></img>
+                    </div>
             </div>
         )
     }
