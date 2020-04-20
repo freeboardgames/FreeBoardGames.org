7b52b2d4ceaba5241381e3f5a73c748e324e2d69
DanielSherlock <d.t.sherlock@live.co.uk>
2020-04-20 10:23:00 +0100

    Adjusted text inside value-hints

--- src/games/zooparade/components/bhinticon.tsx
+++ src/games/zooparade/components/bhinticon.tsx
@@ -22,11 +22,13 @@ export class BHintIcon extends React.Component< InnerWrapper, {}> {
                       width: '15px',
                       display: 'inline-block',
                       boxSizing: 'border-box',
+                      overflow: 'hidden',
                       border: '2.5px solid #572511',
                       borderRadius: '10%',
                       backgroundColor: this.props.hintIcon.color !== -1 ?
                         colors[this.props.hintIcon.color] : 'black',
-                      fontSize: '0.8em'
+                      fontSize: '0.8em',
+                      lineHeight: '0.9'
                 }}
               key={this.props.keyPropagation}
               >
