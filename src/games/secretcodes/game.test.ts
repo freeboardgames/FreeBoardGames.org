4c03accfc3ee0badff6718f559a4a4c125960edc
Joost van Someren <joost.v.someren@gmail.com>
2020-04-25 14:37:18 +0200

    fix secret codes tests

--- src/games/secretcodes/game.test.ts
+++ src/games/secretcodes/game.test.ts
@@ -10,26 +10,31 @@ describe('secret codes rules', () => {
     }) as any;
 
     client.moves.startGame();
+    client.moves.clueGiven();   // blue team starts
+    client.moves.pass();        // pass turn to red team
     client.moves.clueGiven();
     client.moves.chooseCard(0); // "sock", red card, mistake.
     client.moves.clueGiven();
     client.moves.chooseCard(1); // "fish", red card, correct.
     client.moves.chooseCard(2); // "web", red card, correct.
-    client.moves.chooseCard(3); // "lock", blue card, mistake.
+    client.moves.chooseCard(3); // "lock", civilian card, mistake.
     client.moves.clueGiven();
     client.moves.chooseCard(4); // "plate", blue card, correct.
-    client.moves.chooseCard(5); // "capital", civilian card, mistake.
+    client.moves.chooseCard(5); // "capital", blue card, correct.
+    client.moves.pass();
     client.moves.clueGiven();
-    client.moves.chooseCard(6); // "spring", blue card, mistake.
+    client.moves.chooseCard(6); // "spring", civilian card, mistake.
     client.moves.clueGiven();
-    client.moves.chooseCard(7); // "berry", red card, mistake.
+    client.moves.chooseCard(7); // "berry", blue card, correct.
+    client.moves.pass();
     client.moves.clueGiven();
-    client.moves.chooseCard(8); // "tag", blue card, mistake.
+    client.moves.chooseCard(8); // "tag", civilian card, mistake.
     client.moves.clueGiven();
     client.moves.chooseCard(9); // "missile", blue card, correct.
     client.moves.chooseCard(10); // "beijing", blue card, correct.
-    client.moves.chooseCard(17); // "gold", blue card, correct.
-    client.moves.chooseCard(19); // "cloak", blue card, correct. Blue wins.
+    client.moves.chooseCard(13); // "foot", blue card, correct.
+    client.moves.chooseCard(18); // "crane", blue card, correct.
+    client.moves.chooseCard(23); // "australia", blue card, correct. Blue wins.
 
     const ctx: IGameCtx = client.store.getState().ctx;
     expect(ctx.gameover.winner.color).toEqual(TeamColor.Blue);
@@ -41,25 +46,29 @@ describe('secret codes rules', () => {
     }) as any;
 
     client.moves.startGame();
+    client.moves.clueGiven();   // blue team starts
+    client.moves.pass();        // pass turn to red team
     client.moves.clueGiven();
     client.moves.chooseCard(0); // "sock", red card, mistake.
     client.moves.clueGiven();
     client.moves.chooseCard(1); // "fish", red card, correct.
     client.moves.chooseCard(2); // "web", red card, correct.
-    client.moves.chooseCard(3); // "lock", blue card, mistake.
+    client.moves.chooseCard(3); // "lock", civilian card, mistake.
     client.moves.clueGiven();
     client.moves.chooseCard(4); // "plate", blue card, correct.
-    client.moves.chooseCard(5); // "capital", civilian card, mistake.
+    client.moves.chooseCard(5); // "capital", blue card, correct.
+    client.moves.pass();
     client.moves.clueGiven();
-    client.moves.chooseCard(6); // "spring", blue card, mistake.
+    client.moves.chooseCard(6); // "spring", civilian card, mistake.
     client.moves.clueGiven();
-    client.moves.chooseCard(7); // "berry", red card, mistake.
+    client.moves.chooseCard(7); // "berry", blue card, correct.
+    client.moves.pass();
     client.moves.clueGiven();
-    client.moves.chooseCard(8); // "tag", blue card, mistake.
+    client.moves.chooseCard(8); // "tag", civilian card, mistake.
     client.moves.clueGiven();
     client.moves.chooseCard(9); // "missile", blue card, correct.
     client.moves.chooseCard(10); // "beijing", blue card, correct.
-    client.moves.chooseCard(12); // "model", assassin card, loses the game.
+    client.moves.chooseCard(11); // "palm", assassin card, loses the game.
 
     const ctx: IGameCtx = client.store.getState().ctx;
     expect(ctx.gameover.winner.color).toEqual(TeamColor.Red);
