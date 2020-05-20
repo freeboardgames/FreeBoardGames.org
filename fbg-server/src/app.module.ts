80ac4b56bf483e96eeade45eca4b5ee3ccd64929
flamecoals <flamecoals@users.noreply.github.com>
2020-05-19 20:08:55 -0700

    Add support to postgres url from env

--- fbg-server/src/app.module.ts
+++ fbg-server/src/app.module.ts
@@ -3,12 +3,20 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from './users/users.module';
 import { RoomsModule } from './rooms/rooms.module';
 import { MatchModule } from './match/match.module';
+const CONNECTION: any = process.env.POSTGRES_URL
+  ? {
+      type: 'postgres',
+      url: process.env.POSTGRES_URL,
+    }
+  : {
+      type: 'sqlite',
+      database: 'dev.db',
+    };
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
-      type: 'sqlite',
-      database: 'dev.db',
+      ...CONNECTION,
       autoLoadEntities: true,
       synchronize: true,
       logging: false,
