62c9d65768d66192ca08c80711217de6e39649d2
Jason <me@jasonharrison.us>
2020-05-20 02:56:01 -0400

    fix ci

--- web/cypress/plugins/index.js
+++ web/cypress/plugins/index.js
@@ -10,14 +10,14 @@ module.exports = (on, config) => {
   if (config.env.userAgent === 'mobile') {
     // Pixel 2 XL
     configOverride.userAgent =
-    'Mozilla/5.0 (Linux; Android 8.0.0; Pixel 2 XL Build/OPD1.170816.004) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Mobile Safari/537.36';
+      'Mozilla/5.0 (Linux; Android 8.0.0; Pixel 2 XL Build/OPD1.170816.004) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Mobile Safari/537.36';
     configOverride.viewportWidth = 411;
     configOverride.viewportHeight = 823;
     configOverride.testFiles = 'mobile/**/*.*';
   } else if (config.env.userAgent === 'tablet') {
     // iPad Pro
     configOverride.userAgent =
-    'Mozilla/5.0 (iPad; CPU OS 11_0 like Mac OS X) AppleWebKit/604.1.34 (KHTML, like Gecko) Version/11.0 Mobile/15A5341f Safari/604.1'
+      'Mozilla/5.0 (iPad; CPU OS 11_0 like Mac OS X) AppleWebKit/604.1.34 (KHTML, like Gecko) Version/11.0 Mobile/15A5341f Safari/604.1';
     configOverride.viewportWidth = 1024;
     configOverride.viewportHeight = 1366;
     configOverride.testFiles = 'tablet/**/*.*';
