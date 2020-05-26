9e4cad74549dace82443ef3cd2548db7009b9490
Jason <me@jasonharrison.us>
2020-05-25 20:29:42 -0400

    get storybook working

---
+++ web/src/components/App/Game/QrCodeIcon.stories.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import QrCodeIcon from './QrCodeIcon';
+
+export default {
+  title: 'QrCodeIcon',
+  // parameters: { notes: componentNotes },
+};
+
+export const small = () => <QrCodeIcon/>;
