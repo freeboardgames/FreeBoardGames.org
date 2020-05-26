5b78c3fa7e703f01300277c4af2e6d596b31b521
Jason <me@jasonharrison.us>
2020-05-25 21:01:51 -0400

    fix gitignore

--- web/src/components/App/Game/QrCodeIcon.stories.tsx
+++ web/src/components/App/Game/QrCodeIcon.stories.tsx
@@ -1,9 +1,7 @@
-import React from 'react';
 import QrCodeIcon from './QrCodeIcon';
 
 export default {
   title: 'QrCodeIcon',
-  // parameters: { notes: componentNotes },
 };
 
 export const small = () => <QrCodeIcon />;
