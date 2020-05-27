7ae7121cde746847389ced163136f5c07033f66d
Jason <me@jasonharrison.us>
2020-05-26 22:40:03 -0400

    add storysource plugin to storybook

--- web/.storybook/main.js
+++ web/.storybook/main.js
@@ -1,4 +1,4 @@
 module.exports = {
   stories: ['../src/components/**/**/*.stories.tsx'],
-  addons: ['@storybook/addon-actions', '@storybook/addon-links'],
+  addons: ['@storybook/addon-actions', '@storybook/addon-links', '@storybook/addon-storysource'],
 };
