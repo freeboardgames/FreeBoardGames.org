bf021667d886702cd2a991d7aab4ad0f9d2b4de7
flamecoals <flamecoals@users.noreply.github.com>
2020-05-28 23:07:46 -0700

    Update fbg-server

--- fbg-server/src/match/match.resolver.spec.ts
+++ fbg-server/src/match/match.resolver.spec.ts
@@ -1,22 +1,22 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { MatchController } from './match.controller';
+import { MatchResolver } from './match.resolver';
 import { MatchModule } from './match.module';
 import { RoomsModule } from '../rooms/rooms.module';
 import { UsersModule } from '../users/users.module';
 import { FakeDbModule } from '../testing/dbUtil';
 
-describe('Match Controller', () => {
-  let controller: MatchController;
+describe('Match Resolver', () => {
+  let resolver: MatchResolver;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       imports: [FakeDbModule, UsersModule, RoomsModule, MatchModule],
     }).compile();
 
-    controller = module.get<MatchController>(MatchController);
+    resolver = module.get<MatchResolver>(MatchResolver);
   });
 
   it('should be defined', () => {
-    expect(controller).toBeDefined();
+    expect(resolver).toBeDefined();
   });
 });
