fc5f92052556557cd563535b996b977082087d14
flamecoals <flamecoals@users.noreply.github.com>
2020-05-30 23:26:45 -0700

    Syncing with HEAD

--- web/src/games/zooparade/board.tsx
+++ web/src/games/zooparade/board.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
-import { IGameArgs } from '../../components/App/Game/GameBoardWrapper';
-import { GameLayout } from '../../components/App/Game/GameLayout';
+import { IGameArgs } from 'gamesShared/definitions/game';
+import { GameLayout } from 'gamesShared/components/fbg/GameLayout';
 import { IG } from './interfaces';
 
 import { BHand } from './components/bhand';
