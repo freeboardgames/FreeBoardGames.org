4cf4b8b60cbd99d0a78c067c99ca7469c791409f
flamecoals <flamecoals@users.noreply.github.com>
2020-05-31 16:34:43 -0700

    Adding storybook stories for Room

---
+++ web/src/infra/room/GameSharing.stories.tsx
@@ -0,0 +1,8 @@
+import { GameSharing } from './GameSharing';
+
+export default {
+  title: 'Infrastructure/Room/GameSharing',
+};
+
+export const chess = () => <GameSharing gameCode="chess" roomID="foo" />;
+
---
+++ web/src/infra/room/icons/WhatsAppIcon.stories.tsx
@@ -0,0 +1,7 @@
+import WhatsAppIcon from './WhatsAppIcon';
+
+export default {
+  title: 'Infrastructure/Room/Icons/WhatsAppIcon',
+};
+
+export const example = () => <WhatsAppIcon />;
