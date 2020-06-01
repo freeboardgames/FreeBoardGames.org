b509b1cd49a5bb9ba9f6dc0f3d783ec6c87dff7d
flamecoals <flamecoals@users.noreply.github.com>
2020-05-31 21:21:30 -0700

    Fixing CI

--- web/src/gamesShared/components/fbg/GameLayout.stories.disabled.tsx
+++ web/src/gamesShared/components/fbg/GameLayout.stories.disabled.tsx
@@ -5,16 +5,4 @@ export default {
   title: 'Games (shared)/Components/FBG/GameLayout',
 };
 
-const scores = [
-  { playerID: '0', score: 100 },
-  { playerID: '1', score: 200 },
-  { playerID: '2', score: 300 },
-];
-
-const players = [
-  { playerID: 0, name: 'Bob' },
-  { playerID: 1, name: 'Alice' },
-  { playerID: 2, name: 'Joe' },
-];
-
 export const example = () => <GameLayout gameArgs={{ gameCode: 'chess', mode: GameMode.LocalFriend }} />;
