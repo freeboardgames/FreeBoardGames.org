ce4f2a50218b0d40f8a2cfda944953c72bf77db7
flamecoals <flamecoals@users.noreply.github.com>
2020-06-30 19:39:37 -0700

    Fixing timeout to expire rooms

--- fbg-server/src/rooms/constants.ts
+++ fbg-server/src/rooms/constants.ts
@@ -1,2 +1,2 @@
 // How long a membership will last since the last time the user reloaded the page.
-export const EXPIRE_MEMBERSHIP_AFTER_MS = 1 * 1e3; // 60 mins in miliseconds
+export const EXPIRE_MEMBERSHIP_AFTER_MS = 60 * 60 * 1e3; // 60 mins in miliseconds
