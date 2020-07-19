8e41d79efb7dbb003b76a59c8699ebd59b1852cf
flamecoals <flamecoals@users.noreply.github.com>
2020-07-19 16:16:09 -0700

    Fix CI

--- web/src/games/zooparade/components/blog.stories.tsx
+++ web/src/games/zooparade/components/blog.stories.tsx
@@ -1,7 +1,16 @@
 import { BLog } from './blog';
+import { Log, Moves } from '../interfaces';
+import { IPlayerInRoom } from 'gamesShared/definitions/player';
 
 export default {
   title: 'Games/Hanabi/Components/BLog',
 };
 
-export const Example = () => <BLog lines={['Hello', 'World']} keyPropagation={'foo'} />;
+const log: Log[] = [
+  { player: '0', move: Moves.moveDiscard, cardColor: 1, cardValue: 2 },
+  { player: '0', move: Moves.movePlay, cardColor: 3, cardValue: 1, success: false },
+];
+
+const players: IPlayerInRoom[] = [{ name: 'Foo', playerID: 0 }];
+
+export const Example = () => <BLog log={log} players={players} keyPropagation={'foo'} />;
