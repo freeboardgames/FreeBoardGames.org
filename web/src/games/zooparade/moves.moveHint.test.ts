afd34592ebb5693df04e91ab91e12a89a0e58aaa
DanielSherlock <d.t.sherlock@live.co.uk>
2020-08-02 16:20:36 +0100

    Edit bad-hint test to cover new behaviour
    Behaviour 2 (as in #624) now covered by edited test.

--- web/src/games/zooparade/moves.moveHint.test.ts
+++ web/src/games/zooparade/moves.moveHint.test.ts
@@ -358,7 +358,7 @@ it('Hint Value 2 - Hint Value nothing.', () => {
             },
             {
               color: [IHintMask.UNKNOWN, IHintMask.NO, IHintMask.UNKNOWN, IHintMask.UNKNOWN, IHintMask.UNKNOWN],
-              value: [...UNKNOWN_MASK],
+              value: [IHintMask.NO, IHintMask.UNKNOWN, IHintMask.UNKNOWN, IHintMask.NO, IHintMask.NO],
             },
             {
               color: [IHintMask.UNKNOWN, IHintMask.YES, IHintMask.UNKNOWN, IHintMask.UNKNOWN, IHintMask.UNKNOWN],
@@ -489,7 +489,7 @@ it('Hint Value 2 - Hint Value nothing.', () => {
             color: [IHintMask.UNKNOWN, IHintMask.NO, IHintMask.UNKNOWN, IHintMask.UNKNOWN, IHintMask.UNKNOWN],
           },
           {
-            value: [IHintMask.UNKNOWN, IHintMask.UNKNOWN, IHintMask.NO, IHintMask.UNKNOWN, IHintMask.UNKNOWN],
+            value: [IHintMask.NO, IHintMask.YES, IHintMask.NO, IHintMask.NO, IHintMask.NO],
             color: [IHintMask.UNKNOWN, IHintMask.NO, IHintMask.UNKNOWN, IHintMask.UNKNOWN, IHintMask.UNKNOWN],
           },
           {
