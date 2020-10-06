aa451fa712f090636092dc78aafb45eb0a32c4f6
Yannick Saive <ykse999@gmail.com>
2020-10-06 12:03:11 +0200

    update hint colors to be same as card backgrounds

--- web/src/games/zooparade/components/bhinticon.tsx
+++ web/src/games/zooparade/components/bhinticon.tsx
@@ -8,7 +8,7 @@ interface InnerWrapper {
   keyPropagation: string;
 }
 
-const colors = ['#80b28e', '#beb7a3', '#a38c49', '#f2f47d', '#9bcbee'];
+const colors = ['#8bc34a','#9e9e9e','#795548','#ffeb3b','#5677fc','#607d8b'];
 
 export class BHintIcon extends React.Component<InnerWrapper, {}> {
   render() {
