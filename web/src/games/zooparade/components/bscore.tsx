6cc3842d6dd1109eb157d0f1261281c9b053e6d8
flamecoals <flamecoals@users.noreply.github.com>
2020-10-07 02:09:56 +0000

    Improving code and css

--- web/src/games/zooparade/components/bscore.tsx
+++ web/src/games/zooparade/components/bscore.tsx
@@ -1,43 +1,15 @@
 import React from 'react';
 import { ICard } from '../interfaces';
-import css from './bdeck.css';
+import css from './bscore.css';
 
-interface InnerWrapper {
+interface ScoreProps {
   piles: ICard[][];
 }
 
-function getTop(pile: ICard[]): number {
-  // I'm so good at Typescript!
-  if (pile[0] == null) {
-    return 0;
-  }
-  if (pile[1] == null) {
-    return 1;
-  }
-  if (pile[2] == null) {
-    return 2;
-  }
-  if (pile[3] == null) {
-    return 3;
-  }
-  if (pile[4] == null) {
-    return 4;
-  }
-
-  return 5;
-}
-export class BScore extends React.Component<InnerWrapper, {}> {
+export class BScore extends React.Component<ScoreProps, {}> {
   render() {
-    var score = this.props.piles
-      .map((pile) => {
-        return getTop(pile);
-      })
-      .reduce((a, b) => a + b, 0);
+    let score = this.props.piles.map((pile) => pile.filter((card) => card !== null).length).reduce((a, b) => a + b, 0);
 
-    return (
-      <div className={css.wrapper}>
-        <h2 className={css.text}> Score : {score}</h2>
-      </div>
-    );
+    return <h3 className={css.text}>Score: {score}</h3>;
   }
 }
