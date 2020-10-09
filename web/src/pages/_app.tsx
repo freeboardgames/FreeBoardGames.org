acf8055ee4c82398f7beb8595244ddf7dc2791da
flamecoals <flamecoals@users.noreply.github.com>
2020-10-09 03:53:39 +0000

    Updating nextjs, webpack, typescript

--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -1,5 +1,6 @@
 /* eslint-disable react/react-in-jsx-scope */
 
+import Head from 'next/head';
 import App from 'next/app';
 import React from 'react';
 import { ThemeProvider } from '@material-ui/core/styles';
@@ -89,14 +90,28 @@ class defaultApp extends App {
   render() {
     const { Component, pageProps, isMobile } = this.props as any;
     return (
-      <ThemeProvider theme={theme}>
-        <SelfXSSWarning />
-        <UaContext.Provider value={isMobile}>
-          <ApolloProvider client={client}>
-            <Component {...pageProps} />
-          </ApolloProvider>
-        </UaContext.Provider>
-      </ThemeProvider>
+      <>
+        <Head>
+          <meta charSet="utf-8" />
+          <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+          <meta name="mobile-web-app-capable" content="yes" />
+          <meta name="apple-mobile-web-app-capable" content="yes" />
+          <meta name="application-name" content="FreeBoardGames.org" />
+          <meta name="apple-mobile-web-app-title" content="FreeBoardGames.org" />
+          <meta name="theme-color" content="#3f51b5" />
+          <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+          <meta name="msapplication-TileColor" content="#ffc40d" />
+          <meta name="msapplication-config" content="/static/icons/browserconfig.xml" />
+        </Head>
+        <ThemeProvider theme={theme}>
+          <SelfXSSWarning />
+          <UaContext.Provider value={isMobile}>
+            <ApolloProvider client={client}>
+              <Component {...pageProps} />
+            </ApolloProvider>
+          </UaContext.Provider>
+        </ThemeProvider>
+      </>
     );
   }
   static async getInitialProps({ Component, ctx }) {
