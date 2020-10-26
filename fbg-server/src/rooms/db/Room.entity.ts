48775ced2389870e6082b68ff6600b77b75b7ba1
Flamecoals <flamecoals@users.noreply.github.com>
2020-10-25 21:59:58 -0300

    Fix lint warnings on fbg-server

--- fbg-server/src/rooms/db/Room.entity.ts
+++ fbg-server/src/rooms/db/Room.entity.ts
@@ -24,10 +24,10 @@ export class RoomEntity extends BaseEntity {
   @Column()
   isPublic!: boolean;
 
-  @OneToMany((type) => RoomMembershipEntity, (membership) => membership.room)
+  @OneToMany(() => RoomMembershipEntity, (membership) => membership.room)
   public userMemberships!: RoomMembershipEntity[];
 
-  @OneToOne((type) => MatchEntity, (match) => match.room)
+  @OneToOne(() => MatchEntity, (match) => match.room)
   @JoinColumn()
   public match?: MatchEntity;
 }
