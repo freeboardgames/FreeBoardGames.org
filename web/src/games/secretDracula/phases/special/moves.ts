c1124894fc53fb6b4821cb4f9ebaf9f31ae15fd8
Yannick Saive <ykse999@gmail.com>
2020-10-31 22:54:46 +0100

    fix

--- web/src/games/secretDracula/phases/special/moves.ts
+++ web/src/games/secretDracula/phases/special/moves.ts
@@ -6,7 +6,7 @@ export function movePickMayor(G: IG, ctx: Ctx, id: number, me: number): IG | 'IN
   if (id == undefined) {
     return INVALID_MOVE;
   }
-  if (id < 0 || ctx.numPlayers > 2) {
+  if (id < 0 || ctx.numPlayers < id) {
     return INVALID_MOVE;
   }
   if (G.deadIDs.includes(id)) {
