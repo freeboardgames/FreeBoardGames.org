3e679ae979e0ceef27b2274d530a0f298fa54c29
Lee Kellogg <lfkellogg@users.noreply.github.com>
2020-11-08 21:45:24 -0500

    Initial commit - Mergers (game only) (#663)

---
+++ web/src/games/mergers/index.ts
@@ -0,0 +1,23 @@
+const Thumbnail = require('./media/thumbnail.png?lqip-colors');
+import { GameMode } from 'gamesShared/definitions/mode';
+import { IGameDef, IGameStatus } from 'gamesShared/definitions/game';
+import instructions from './instructions.md';
+
+export const mergersGameDef: IGameDef = {
+  code: 'mergers',
+  name: 'Mergers',
+  imageURL: Thumbnail,
+  modes: [{ mode: GameMode.OnlineFriend }],
+  minPlayers: 3,
+  maxPlayers: 6,
+  description: 'Similar to Acquire',
+  descriptionTag: 'Invest in hotels and dominate the market!',
+  instructions: {
+    videoId: 'AVoUPu5O1os',
+    text: instructions,
+  },
+  status: IGameStatus.IN_DEVELOPMENT,
+  config: () => import('./config'),
+};
+
+export default mergersGameDef;
