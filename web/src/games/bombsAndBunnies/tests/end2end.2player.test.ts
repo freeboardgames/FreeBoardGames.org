6a144906a221088e844ff9d8f232381ec6228b9e
b-hub <ben.magistris@gmail.com>
2020-11-27 13:08:30 +0000

    add more full game play tests

--- web/src/games/bombsAndBunnies/tests/end2end.2player.test.ts
+++ web/src/games/bombsAndBunnies/tests/end2end.2player.test.ts
@@ -3,7 +3,7 @@ import { BombsAndBunniesGame } from '../game';
 import { Client } from 'boardgame.io/client';
 import { Local } from 'boardgame.io/multiplayer';
 import { _ClientImpl } from 'boardgame.io/dist/types/src/client/client';
-import { getTypedMoves, getTypedState } from './utilities';
+import * as Utilities from './utilities';
 
 let _matchId = 0;
 
@@ -30,30 +30,30 @@ function setup(): _ClientImpl<any>[] {
 it('p1 consecutive wins with early betting', () => {
   const [p1, p2] = setup();
 
-  getTypedMoves(p1).PlaceCard(0);
-  getTypedMoves(p2).PlaceCard(0);
+  Utilities.getTypedMoves(p1).PlaceCard(0);
+  Utilities.getTypedMoves(p2).PlaceCard(0);
 
-  getTypedMoves(p1).Bet(1);
-  getTypedMoves(p2).SkipBet();
+  Utilities.getTypedMoves(p1).Bet(1);
+  Utilities.getTypedMoves(p2).SkipBet();
 
-  getTypedMoves(p1).Reveal(p1.playerID);
+  Utilities.getTypedMoves(p1).Reveal(p1.playerID);
 
-  var { G, ctx } = getTypedState(p1);
+  var { G, ctx } = Utilities.getTypedState(p1);
 
   expect(G.players[0].wins).toEqual(1);
   expect(ctx.phase).toEqual(Phases.initial_placement);
   expect(ctx.currentPlayer).toEqual(p2.playerID);
 
-  getTypedMoves(p2).PlaceCard(0);
-  getTypedMoves(p1).PlaceCard(0);
+  Utilities.getTypedMoves(p2).PlaceCard(0);
+  Utilities.getTypedMoves(p1).PlaceCard(0);
 
-  getTypedMoves(p2).Bet(1);
-  getTypedMoves(p1).Bet(2);
+  Utilities.getTypedMoves(p2).Bet(1);
+  Utilities.getTypedMoves(p1).Bet(2);
 
-  getTypedMoves(p1).Reveal(p1.playerID);
-  getTypedMoves(p1).Reveal(p2.playerID);
+  Utilities.getTypedMoves(p1).Reveal(p1.playerID);
+  Utilities.getTypedMoves(p1).Reveal(p2.playerID);
 
-  var { G, ctx } = getTypedState(p1);
+  var { G, ctx } = Utilities.getTypedState(p1);
   expect(G.players[0].wins).toEqual(2);
   expect(ctx.gameover).toEqual({ winner: p1.playerID });
   return;
@@ -62,32 +62,126 @@ it('p1 consecutive wins with early betting', () => {
 it('p2 consecutive wins with early betting', () => {
   const [p1, p2] = setup();
 
-  getTypedMoves(p1).PlaceCard(0);
-  getTypedMoves(p2).PlaceCard(0);
+  Utilities.getTypedMoves(p1).PlaceCard(0);
+  Utilities.getTypedMoves(p2).PlaceCard(0);
 
-  getTypedMoves(p1).Bet(1);
-  getTypedMoves(p2).Bet(2);
+  Utilities.getTypedMoves(p1).Bet(1);
+  Utilities.getTypedMoves(p2).Bet(2);
 
-  getTypedMoves(p2).Reveal(p2.playerID);
-  getTypedMoves(p2).Reveal(p1.playerID);
+  Utilities.getTypedMoves(p2).Reveal(p2.playerID);
+  Utilities.getTypedMoves(p2).Reveal(p1.playerID);
 
-  var { G, ctx } = getTypedState(p2);
+  var { G, ctx } = Utilities.getTypedState(p2);
 
   expect(G.players[1].wins).toEqual(1);
   expect(ctx.phase).toEqual(Phases.initial_placement);
   expect(ctx.currentPlayer).toEqual(p1.playerID);
 
-  getTypedMoves(p1).PlaceCard(0);
-  getTypedMoves(p2).PlaceCard(0);
+  Utilities.getTypedMoves(p1).PlaceCard(0);
+  Utilities.getTypedMoves(p2).PlaceCard(0);
 
-  getTypedMoves(p1).Bet(1);
-  getTypedMoves(p2).Bet(2);
+  Utilities.getTypedMoves(p1).Bet(1);
+  Utilities.getTypedMoves(p2).Bet(2);
 
-  getTypedMoves(p2).Reveal(p2.playerID);
-  getTypedMoves(p2).Reveal(p1.playerID);
+  Utilities.getTypedMoves(p2).Reveal(p2.playerID);
+  Utilities.getTypedMoves(p2).Reveal(p1.playerID);
 
-  var { G, ctx } = getTypedState(p2);
+  var { G, ctx } = Utilities.getTypedState(p2);
   expect(G.players[1].wins).toEqual(2);
+  expect(ctx.gameover).toEqual({ winner: p2.playerID });
+  return;
+});
+
+it('p2 wins by default after p1 picks their own bomb every time', () => {
+  const [p1, p2] = setup();
+
+  Utilities.PlaceBombCard(p1);
+  Utilities.PlaceBombCard(p2);
+
+  Utilities.getTypedMoves(p1).Bet(1);
+  Utilities.getTypedMoves(p2).SkipBet();
+
+  Utilities.getTypedMoves(p1).Reveal(p1.playerID);
+
+  var { G, ctx } = Utilities.getTypedState(p1);
+
+  expect(G.bombPlayerId).toEqual(p1.playerID);
+  expect(G.failedRevealPlayerId).toEqual(p1.playerID);
+  expect(ctx.phase).toEqual(Phases.penalty);
+  expect(ctx.currentPlayer).toEqual(p1.playerID); // p1 chooses a card to discard
+
+  Utilities.DiscardOwnBunnyCard(p1);
+
+  // -- Round 2 ------------------------------------------
+  var { G, ctx } = Utilities.getTypedState(p1);
+
+  expect(ctx.phase).toEqual(Phases.initial_placement);
+  expect(ctx.currentPlayer).toEqual(p1.playerID); // player who had the bomb starts
+
+  Utilities.PlaceBombCard(p1);
+  Utilities.PlaceBombCard(p2);
+
+  Utilities.getTypedMoves(p1).Bet(1);
+  Utilities.getTypedMoves(p2).SkipBet();
+
+  Utilities.getTypedMoves(p1).Reveal(p1.playerID);
+
+  var { G, ctx } = Utilities.getTypedState(p1);
+
+  expect(G.bombPlayerId).toEqual(p1.playerID);
+  expect(G.failedRevealPlayerId).toEqual(p1.playerID);
+  expect(ctx.phase).toEqual(Phases.penalty);
+  expect(ctx.currentPlayer).toEqual(p1.playerID); // p1 chooses a card to discard
+
+  Utilities.DiscardOwnBunnyCard(p1);
+
+  // -- Round 3 ------------------------------------------
+  var { G, ctx } = Utilities.getTypedState(p1);
+
+  expect(ctx.phase).toEqual(Phases.initial_placement);
+  expect(ctx.currentPlayer).toEqual(p1.playerID); // player who had the bomb starts
+
+  Utilities.PlaceBombCard(p1);
+  Utilities.PlaceBombCard(p2);
+
+  Utilities.getTypedMoves(p1).Bet(1);
+  Utilities.getTypedMoves(p2).SkipBet();
+
+  Utilities.getTypedMoves(p1).Reveal(p1.playerID);
+
+  var { G, ctx } = Utilities.getTypedState(p1);
+
+  expect(G.bombPlayerId).toEqual(p1.playerID);
+  expect(G.failedRevealPlayerId).toEqual(p1.playerID);
+  expect(ctx.phase).toEqual(Phases.penalty);
+  expect(ctx.currentPlayer).toEqual(p1.playerID); // p1 chooses a card to discard
+
+  Utilities.DiscardOwnBunnyCard(p1);
+
+  // -- Round 4 ------------------------------------------
+  var { G, ctx } = Utilities.getTypedState(p1);
+
+  expect(ctx.phase).toEqual(Phases.initial_placement);
+  expect(ctx.currentPlayer).toEqual(p1.playerID); // player who had the bomb starts
+
+  Utilities.PlaceBombCard(p1);
+  Utilities.PlaceBombCard(p2);
+
+  Utilities.getTypedMoves(p1).Bet(1);
+  Utilities.getTypedMoves(p2).SkipBet();
+
+  Utilities.getTypedMoves(p1).Reveal(p1.playerID);
+
+  var { G, ctx } = Utilities.getTypedState(p1);
+
+  expect(G.bombPlayerId).toEqual(p1.playerID);
+  expect(G.failedRevealPlayerId).toEqual(p1.playerID);
+  expect(ctx.phase).toEqual(Phases.penalty);
+  expect(ctx.currentPlayer).toEqual(p1.playerID); // p1 chooses a card to discard
+
+  Utilities.DiscardOwnBombCard(p1); // discard's final card which will be the bomb.
+
+  var { G, ctx } = Utilities.getTypedState(p1);
   expect(ctx.gameover).toEqual({ winner: p2.playerID });
   return;
 });
--- web/src/games/bombsAndBunnies/tests/utilities.ts
+++ web/src/games/bombsAndBunnies/tests/utilities.ts
@@ -1,6 +1,7 @@
 import { Ctx } from 'boardgame.io';
 import { _ClientImpl } from 'boardgame.io/dist/types/src/client/client';
-import { IG, IGameMoves } from '../engine/interfaces';
+import { IG, IGameMoves, CardType } from '../engine/interfaces';
+import { getPlayerById } from '../engine/stateExtensions';
 
 export function getTypedMoves(player: _ClientImpl<any>): IGameMoves {
   return player.moves as any;
@@ -8,4 +9,34 @@ export function getTypedMoves(player: _ClientImpl<any>): IGameMoves {
 
 export function getTypedState(player: _ClientImpl<any>): { G: IG; ctx: Ctx } {
   return player.getState();
+}
+
+export function getBombHandIndex(player: _ClientImpl<any>): number {
+  var { G } = getTypedState(player);
+
+  var gamePlayer = getPlayerById(G, player.playerID);
+  return gamePlayer.hand.findIndex((c) => c == CardType.Bomb);
+}
+
+export function getBunnyHandIndex(player: _ClientImpl<any>): number {
+  var { G } = getTypedState(player);
+
+  var gamePlayer = getPlayerById(G, player.playerID);
+  return gamePlayer.hand.findIndex((c) => c == CardType.Bunny);
+}
+
+export function PlaceBunnyCard(player: _ClientImpl<any>): void {
+  getTypedMoves(player).PlaceCard(getBunnyHandIndex(player));
+}
+
+export function PlaceBombCard(player: _ClientImpl<any>): void {
+  getTypedMoves(player).PlaceCard(getBombHandIndex(player));
+}
+
+export function DiscardOwnBunnyCard(player: _ClientImpl<any>): void {
+  getTypedMoves(player).Discard(player.playerID, getBunnyHandIndex(player));
+}
+
+export function DiscardOwnBombCard(player: _ClientImpl<any>): void {
+  getTypedMoves(player).Discard(player.playerID, getBombHandIndex(player));
 }
