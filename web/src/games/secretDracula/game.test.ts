50da60f29edc04ae7c1503e15f669bbde1c5e6ec
Yannick Spooky Saive <ykse999@gmail.com>
2020-12-11 15:21:42 +0100

    fix dead Mayor after No Vote

--- web/src/games/secretDracula/game.test.ts
+++ web/src/games/secretDracula/game.test.ts
@@ -889,3 +889,282 @@ it('end2end - 10 player dracula priest on 3 blood', () => {
   expect(ctx.gameover).toEqual({ lose: true });
   return;
 });
+
+
+it('end2end - 10 NoVote into Dead Mayor', () => {
+  // set up a specific board scenario
+  const SecretDraculaCustomScenario = {
+    ...SecretDraculaGame,
+    numPlayers: 10,
+    setup: (ctx: Ctx) => {
+      return {
+        ..._setup(ctx, false, false),
+      };
+    },
+  };
+
+  const spec = {
+    game: SecretDraculaCustomScenario,
+    numPlayers: 10,
+    multiplayer: Local(),
+  };
+
+  const players = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9].map((value) => {
+    return Client({ ...spec, playerID: value.toString() } as any);
+  });
+
+  players.map((p) => p.start());
+
+  players[0].moves.moveChosePriest(3, 0);
+  var { G, ctx } = players[0].getState();
+  expect(ctx.phase).toEqual('phaseVotePriest');
+  players.map((p, i) => p.moves.moveVoteYes(i));
+  players.map((p, i) => p.moves.moveOKVote(i));
+  players[0].moves.moveDiscardMayor(0, 0);
+  players[3].moves.moveDiscardPriest(0, 3); // -- play blood 1
+
+  var { G, ctx } = players[0].getState();
+  expect(ctx.phase).toEqual('phaseInvestigate1');
+  players[0].moves.moveInvestigateStart(0, 0); // -- Can't Investigate Self
+  var { G, ctx } = players[0].getState();
+  expect(ctx.phase).toEqual('phaseInvestigate1');
+  players[0].moves.moveInvestigateStart(3, 0); //
+  var { G, ctx } = players[0].getState();
+  expect(G.investigate).toEqual(1);
+  expect(G.investigateID).toEqual(3);
+  expect(ctx.phase).toEqual('phaseInvestigate2');
+  players[0].moves.moveInvestigateEnd(0); //
+  var { G, ctx } = players[0].getState();
+  expect(ctx.phase).toEqual('phaseChosePriest');
+
+  players[1].moves.moveChosePriest(3, 1); // -- fail
+  var { G, ctx } = players[0].getState();
+  expect(ctx.phase).toEqual('phaseChosePriest');
+  players[1].moves.moveChosePriest(2, 1);
+  players.map((p, i) => p.moves.moveVoteYes(i));
+  players.map((p, i) => p.moves.moveOKVote(i));
+  players[1].moves.moveDiscardMayor(0, 1);
+  players[2].moves.moveDiscardPriest(0, 2); // -- play blood 2
+
+  var { G, ctx } = players[0].getState();
+  expect(ctx.phase).toEqual('phaseInvestigate1');
+  players[1].moves.moveInvestigateStart(1, 1); // -- Can't Investigate Self
+  var { G, ctx } = players[0].getState();
+  expect(ctx.phase).toEqual('phaseInvestigate1');
+  players[1].moves.moveInvestigateStart(6, 1); //
+  var { G, ctx } = players[1].getState();
+  expect(G.investigate).toEqual(-1);
+  expect(G.investigateID).toEqual(6);
+  expect(ctx.phase).toEqual('phaseInvestigate2');
+  players[1].moves.moveInvestigateEnd(1); //
+  var { G, ctx } = players[0].getState();
+  expect(ctx.phase).toEqual('phaseChosePriest');
+
+  players[2].moves.moveChosePriest(0, 2);
+  players.map((p, i) => p.moves.moveVoteYes(i));
+  players.map((p, i) => p.moves.moveOKVote(i));
+  players[2].moves.moveDiscardMayor(2, 2);
+  players[0].moves.moveDiscardPriest(1, 0); // -- play blood 3
+
+  // Player 2 Mayor
+  // Player 0 Priest
+
+  var { G, ctx } = players[0].getState();
+  expect(ctx.phase).toEqual('phaseSpecialElection');
+  players[2].moves.movePickMayor(7, 2); // -- Can chose last Priest
+  var { G, ctx } = players[0].getState();
+  expect(ctx.phase).toEqual('phaseChosePriest');
+  players[7].moves.moveChosePriest(1, 7);
+
+  // Player 2 Mayor
+  // Player 0 Priest
+  // Player 7 SpecialMayor
+  // Player 1 SpecialPriest
+
+  var { G, ctx } = players[0].getState();
+  expect(ctx.phase).toEqual('phaseVotePriest');
+  players.map((p, i) => p.moves.moveVoteNo(i));
+  players.map((p, i) => p.moves.moveOKVote(i));
+
+  var { G, ctx } = players[0].getState();
+  expect(ctx.phase).toEqual('phaseChosePriest'); // -- next mayor is 3
+  expect(ctx.activePlayers).toEqual({ '3': 'phaseChosePriest' });
+  players[3].moves.moveChosePriest(1, 3);
+  var { G, ctx } = players[0].getState();
+  expect(ctx.phase).toEqual('phaseVotePriest');
+  players.map((p, i) => p.moves.moveVoteNo(i));
+  players.map((p, i) => p.moves.moveOKVote(i));
+
+  var { G, ctx } = players[0].getState();
+  expect(ctx.activePlayers).toEqual({ '4': 'phaseChosePriest' });
+  players[4].moves.moveChosePriest(1, 4);
+  var { G, ctx } = players[0].getState();
+  expect(ctx.phase).toEqual('phaseVotePriest');
+  players.map((p, i) => p.moves.moveVoteNo(i));
+  players.map((p, i) => p.moves.moveOKVote(i)); // -- third NO
+
+  var { G, ctx } = players[0].getState();
+  expect(G.policyBoardVampire.length).toEqual(4); // auto played 4th, but skipped action
+  expect(G.electionTracker).toEqual(0);
+  expect(ctx.activePlayers).toEqual({ '5': 'phaseChosePriest' });
+  players[5].moves.moveChosePriest(2, 5); // can chose old elected mayor, because of electionCounter reset
+  var { G, ctx } = players[0].getState();
+  expect(ctx.phase).toEqual('phaseVotePriest');
+  players.map((p, i) => p.moves.moveVoteYes(i));
+  players.map((p, i) => p.moves.moveOKVote(i));
+  players[5].moves.moveDiscardMayor(2, 5);
+  players[2].moves.moveDiscardPriest(1, 2); // -- play blood 5
+
+  var { G, ctx } = players[0].getState();
+  expect(ctx.phase).toEqual('phaseExecution');
+  players[5].moves.moveExecute(7, 5); // -- execute following mayor
+
+  var { G, ctx } = players[0].getState();
+  expect(ctx.activePlayers).toEqual({ '6': 'phaseChosePriest' });
+  players[6].moves.moveChosePriest(1, 6);
+  var { G, ctx } = players[0].getState();
+  expect(ctx.phase).toEqual('phaseVotePriest');
+  players.map((p, i) => p.moves.moveVoteNo(i));
+  players.map((p, i) => p.moves.moveOKVote(i));
+
+  // should have skipped 7 as Mayor, because Dead
+  var { G, ctx } = players[0].getState();
+  expect(ctx.activePlayers).toEqual({ '8': 'phaseChosePriest' });
+  return;
+});
+
+
+it('end2end - 10 execute the following mayor', () => {
+  // set up a specific board scenario
+  const SecretDraculaCustomScenario = {
+    ...SecretDraculaGame,
+    numPlayers: 10,
+    setup: (ctx: Ctx) => {
+      return {
+        ..._setup(ctx, false, false),
+      };
+    },
+  };
+
+  const spec = {
+    game: SecretDraculaCustomScenario,
+    numPlayers: 10,
+    multiplayer: Local(),
+  };
+
+  const players = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9].map((value) => {
+    return Client({ ...spec, playerID: value.toString() } as any);
+  });
+
+  players.map((p) => p.start());
+
+  players[0].moves.moveChosePriest(3, 0);
+  var { G, ctx } = players[0].getState();
+  expect(ctx.phase).toEqual('phaseVotePriest');
+  players.map((p, i) => p.moves.moveVoteYes(i));
+  players.map((p, i) => p.moves.moveOKVote(i));
+  players[0].moves.moveDiscardMayor(0, 0);
+  players[3].moves.moveDiscardPriest(0, 3); // -- play blood 1
+
+  var { G, ctx } = players[0].getState();
+  expect(ctx.phase).toEqual('phaseInvestigate1');
+  players[0].moves.moveInvestigateStart(0, 0); // -- Can't Investigate Self
+  var { G, ctx } = players[0].getState();
+  expect(ctx.phase).toEqual('phaseInvestigate1');
+  players[0].moves.moveInvestigateStart(3, 0); //
+  var { G, ctx } = players[0].getState();
+  expect(G.investigate).toEqual(1);
+  expect(G.investigateID).toEqual(3);
+  expect(ctx.phase).toEqual('phaseInvestigate2');
+  players[0].moves.moveInvestigateEnd(0); //
+  var { G, ctx } = players[0].getState();
+  expect(ctx.phase).toEqual('phaseChosePriest');
+
+  players[1].moves.moveChosePriest(3, 1); // -- fail
+  var { G, ctx } = players[0].getState();
+  expect(ctx.phase).toEqual('phaseChosePriest');
+  players[1].moves.moveChosePriest(2, 1);
+  players.map((p, i) => p.moves.moveVoteYes(i));
+  players.map((p, i) => p.moves.moveOKVote(i));
+  players[1].moves.moveDiscardMayor(0, 1);
+  players[2].moves.moveDiscardPriest(0, 2); // -- play blood 2
+
+  var { G, ctx } = players[0].getState();
+  expect(ctx.phase).toEqual('phaseInvestigate1');
+  players[1].moves.moveInvestigateStart(1, 1); // -- Can't Investigate Self
+  var { G, ctx } = players[0].getState();
+  expect(ctx.phase).toEqual('phaseInvestigate1');
+  players[1].moves.moveInvestigateStart(6, 1); //
+  var { G, ctx } = players[1].getState();
+  expect(G.investigate).toEqual(-1);
+  expect(G.investigateID).toEqual(6);
+  expect(ctx.phase).toEqual('phaseInvestigate2');
+  players[1].moves.moveInvestigateEnd(1); //
+  var { G, ctx } = players[0].getState();
+  expect(ctx.phase).toEqual('phaseChosePriest');
+
+  players[2].moves.moveChosePriest(0, 2);
+  players.map((p, i) => p.moves.moveVoteYes(i));
+  players.map((p, i) => p.moves.moveOKVote(i));
+  players[2].moves.moveDiscardMayor(2, 2);
+  players[0].moves.moveDiscardPriest(1, 0); // -- play blood 3
+
+  // Player 2 Mayor
+  // Player 0 Priest
+
+  var { G, ctx } = players[0].getState();
+  expect(ctx.phase).toEqual('phaseSpecialElection');
+  players[2].moves.movePickMayor(7, 2); // -- Can chose last Priest
+  var { G, ctx } = players[0].getState();
+  expect(ctx.phase).toEqual('phaseChosePriest');
+  players[7].moves.moveChosePriest(1, 7);
+
+  // Player 2 Mayor
+  // Player 0 Priest
+  // Player 7 SpecialMayor
+  // Player 1 SpecialPriest
+
+  var { G, ctx } = players[0].getState();
+  expect(ctx.phase).toEqual('phaseVotePriest');
+  players.map((p, i) => p.moves.moveVoteNo(i));
+  players.map((p, i) => p.moves.moveOKVote(i));
+
+  var { G, ctx } = players[0].getState();
+  expect(ctx.phase).toEqual('phaseChosePriest'); // -- next mayor is 3
+  expect(ctx.activePlayers).toEqual({ '3': 'phaseChosePriest' });
+  players[3].moves.moveChosePriest(1, 3);
+  var { G, ctx } = players[0].getState();
+  expect(ctx.phase).toEqual('phaseVotePriest');
+  players.map((p, i) => p.moves.moveVoteNo(i));
+  players.map((p, i) => p.moves.moveOKVote(i));
+
+  var { G, ctx } = players[0].getState();
+  expect(ctx.activePlayers).toEqual({ '4': 'phaseChosePriest' });
+  players[4].moves.moveChosePriest(1, 4);
+  var { G, ctx } = players[0].getState();
+  expect(ctx.phase).toEqual('phaseVotePriest');
+  players.map((p, i) => p.moves.moveVoteNo(i));
+  players.map((p, i) => p.moves.moveOKVote(i)); // -- third NO
+
+  var { G, ctx } = players[0].getState();
+  expect(G.policyBoardVampire.length).toEqual(4); // auto played 4th, but skipped action
+  expect(G.electionTracker).toEqual(0);
+  expect(ctx.activePlayers).toEqual({ '5': 'phaseChosePriest' });
+  players[5].moves.moveChosePriest(2, 5); // can chose old elected mayor, because of electionCounter reset
+  var { G, ctx } = players[0].getState();
+  expect(ctx.phase).toEqual('phaseVotePriest');
+  players.map((p, i) => p.moves.moveVoteYes(i));
+  players.map((p, i) => p.moves.moveOKVote(i));
+  players[5].moves.moveDiscardMayor(2, 5);
+  players[2].moves.moveDiscardPriest(1, 2); // -- play blood 5
+
+  var { G, ctx } = players[0].getState();
+  expect(ctx.phase).toEqual('phaseExecution');
+  players[5].moves.moveExecute(6, 5); // -- execute following mayor
+
+  var { G, ctx } = players[0].getState();
+  expect(ctx.activePlayers).toEqual({ '7': 'phaseChosePriest' });
+  players[4].moves.moveChosePriest(1, 4);
+  var { G, ctx } = players[0].getState();
+  return;
+});
--- web/src/games/secretDracula/game.ts
+++ web/src/games/secretDracula/game.ts
@@ -226,25 +226,23 @@ export const SecretDraculaGame = {
 
           G.lastMayorID = -1; // any mayor priest combi allowed again.
           G.lastPriestID = -1;
-          if (G.specialElection != -1) {
-            G.mayorID = G.specialElection;
-            G.mayorID = (G.mayorID + 1) % ctx.numPlayers; // chose next mayor
-            G.specialElection = -1;
-          } else {
-            G.mayorID = (G.mayorID + 1) % ctx.numPlayers; // chose next mayor
-          }
-          G.priestID = -1; // no active priest
         } else {
           G.electionTracker += 1;
-          if (G.specialElection != -1) {
-            G.mayorID = G.specialElection;
-            G.mayorID = (G.mayorID + 1) % ctx.numPlayers; // chose next mayor
-            G.specialElection = -1;
-          } else {
-            G.mayorID = (G.mayorID + 1) % ctx.numPlayers; // chose next mayor
-          }
-          G.priestID = -1; // no active priest
         }
+
+        if (G.specialElection != -1) {
+          G.mayorID = G.specialElection;
+          G.mayorID = (G.mayorID + 1) % ctx.numPlayers; // chose next mayor
+          G.specialElection = -1;
+        } else {
+          G.mayorID = (G.mayorID + 1) % ctx.numPlayers; // chose next mayor
+        }
+
+        while (G.deadIDs.includes(G.mayorID)){
+          G.mayorID = (G.mayorID + 1) % ctx.numPlayers; // chose next mayor
+        }
+
+        G.priestID = -1; // no active priest
 
         return G;
       },
