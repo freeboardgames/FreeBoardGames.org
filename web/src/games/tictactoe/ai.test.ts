4659cdd52b6ca5898bd9be18f769a28a02ea10d3
flamecoals <flamecoals@users.noreply.github.com>
2021-01-04 16:56:36 -0800

    Fixing tictactoe and tictactoeplus tests

--- web/src/games/tictactoe/ai.test.ts
+++ web/src/games/tictactoe/ai.test.ts
@@ -2,14 +2,23 @@ import AIConfig from './ai';
 import { Step, MCTSBot } from 'boardgame.io/ai';
 import { TictactoeGame } from './game';
 import { Client } from 'boardgame.io/client';
+import { GameCustommizationState } from 'gamesShared/definitions/customization';
+import { TicTacToeDifficulty, QuickCustomizationState } from './customization';
+
+function customizationState(difficulty: TicTacToeDifficulty): GameCustommizationState {
+  const quick: QuickCustomizationState = { difficulty };
+  return {
+    quick,
+  };
+}
 
 test('Easy AI - should return a move for the initial position', async () => {
+  const aiConfig = AIConfig.bgioAI(customizationState(TicTacToeDifficulty.EASY));
   const client = Client({
     game: TictactoeGame,
-    ai: AIConfig.bgioAI('1'),
   });
   client.moves.clickCell(0); // player plays
-  await Step(client, new (AIConfig.bgioAI('1').ai)()); // AI plays
+  await Step(client, new aiConfig.ai()); // AI plays
   const { G } = client.store.getState();
   const player0Moves = G.cells.filter((cell: string) => cell === '0');
   const player1Moves = G.cells.filter((cell: string) => cell === '1');
@@ -18,12 +27,12 @@ test('Easy AI - should return a move for the initial position', async () => {
 });
 
 test('Hard AI - should return a move for the initial position', async () => {
+  const aiConfig = AIConfig.bgioAI(customizationState(TicTacToeDifficulty.HARD));
   const client = Client({
     game: TictactoeGame,
-    ai: AIConfig.bgioAI('2'),
   });
   client.moves.clickCell(0); // player plays
-  const bot = new MCTSBot({ game: TictactoeGame, enumerate: AIConfig.bgioAI('2').ai.enumerate });
+  const bot = new MCTSBot({ game: TictactoeGame, enumerate: aiConfig.ai.enumerate });
   await Step(client, bot); // AI plays
   const { G } = client.store.getState();
   const player0Moves = G.cells.filter((cell: string) => cell === '0');
