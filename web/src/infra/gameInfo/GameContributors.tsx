afb29c8bd90cb13afdeec7b13997756dc1d7aca7
flamecoals <flamecoals@users.noreply.github.com>
2021-01-24 15:38:17 -0800

    Address review

--- web/src/infra/gameInfo/GameContributors.tsx
+++ web/src/infra/gameInfo/GameContributors.tsx
@@ -6,30 +6,27 @@ interface GameContributorsProps {
   game: IGameDef;
 }
 
-
-export class GameContributors extends React.Component<GameContributorsProps, {}> {
-  render() {
-    const contributors = this.props.game.contributors.map((username) => (
-      <a 
-        href={`https://github.com/${username}`} 
-        key={username} 
-        style={{marginRight: '4px'}} 
-        target="_blank">
-        <Typography variant="body2" component="span" style={{color: 'blue'}}>
-          {username}
-        </Typography>
-      </a>
-    ));
-    return (
-      <div style={{display: 'flex', height: '24px', maxWidth: '500px'}}>
-        <div style={{flexGrow: 1}}></div>
-        <Typography variant="body2" component="span" style={{marginRight: '4px'}}>
-          by
-        </Typography>
-        <div>
-          {contributors}
-        </div>
-      </div>
-    );
-  }
+export function GameContributors({ game }: GameContributorsProps) {
+  const contributors = game.contributors.map((username) => (
+    <a
+      href={`https://github.com/${username}`}
+      key={username}
+      style={{ marginRight: '4px' }}
+      target="_blank"
+      rel="noreferrer"
+    >
+      <Typography variant="body2" component="span" style={{ color: 'blue' }}>
+        {username}
+      </Typography>
+    </a>
+  ));
+  return (
+    <div style={{ display: 'flex', height: '24px', maxWidth: '500px' }}>
+      <div style={{ flexGrow: 1 }}></div>
+      <Typography variant="body2" component="span" style={{ marginRight: '4px' }}>
+        by
+      </Typography>
+      <div>{contributors}</div>
+    </div>
+  );
 }
