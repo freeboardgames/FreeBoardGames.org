5273cbd4954c181561106f278fff69afbcd8e80e
flamecoals <flamecoals@users.noreply.github.com>
2021-04-19 21:01:19 -0700

    Includes a database check on fbg-server healthz

--- fbg-server/src/healthz.controller.spec.ts
+++ fbg-server/src/healthz.controller.spec.ts
@@ -1,22 +1,32 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { HealthzController } from './healthz.controller';
+import { FakeDbModule, closeDbConnection } from './testing/dbUtil';
+import { RoomsModule } from './rooms/rooms.module';
+import { UsersModule } from './users/users.module';
+import { MatchModule } from './match/match.module';
 
 describe('Healthz Controller', () => {
   let controller: HealthzController;
+  let module: TestingModule;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
+  beforeAll(async () => {
+    module = await Test.createTestingModule({
+      imports: [FakeDbModule, UsersModule, RoomsModule, MatchModule],
       controllers: [HealthzController],
     }).compile();
 
     controller = module.get<HealthzController>(HealthzController);
   });
 
+  afterAll(async () => {
+    closeDbConnection(module);
+  });
+
   it('should be defined', () => {
     expect(controller).toBeDefined();
   });
 
-  it('should return OK', () => {
-    expect(controller.healthz()).toEqual('OK');
+  it('should return OK', async () => {
+    expect(await controller.healthz()).toEqual('OK');
   });
 });
