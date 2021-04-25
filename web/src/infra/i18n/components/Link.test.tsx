e505e47f0e5d6c7e8d04b7b9073625b7a324f0c2
Richard <richard.lopes92@gmail.com>
2021-04-25 13:40:58 -0300

    test: mocking useContext on Link test

--- web/src/infra/i18n/components/Link.test.tsx
+++ web/src/infra/i18n/components/Link.test.tsx
@@ -1,60 +1,64 @@
+import React from 'react';
 import '@testing-library/jest-dom/extend-expect';
 import * as RTL from '@testing-library/react';
 import { screen, waitFor } from '@testing-library/react';
-import React from 'react';
 import { Link } from './Link';
-
-import { nextI18Next } from 'infra/i18n';
-import { mocked } from 'ts-jest/utils';
 import { LinkProps } from 'next/link';
-
-jest.mock('infra/i18n/config', () => {
-  const NextLink = jest.requireActual('next/link').default;
-  return {
-    nextI18Next: {
-      Link: NextLink,
-      useTranslation: jest.fn(),
-    },
-  };
-});
-
-const mockedNextI18Next = mocked(nextI18Next, true);
+import { nextI18Next } from '../config';
+import { mock, mockDeep } from 'jest-mock-extended';
+import { UseTranslation } from 'next-i18next';
+import { mocked } from 'ts-jest/utils';
 
 describe('Link', () => {
-  describe('when translated route is available', () => {
-    it('replaces the href property for it ', async () => {
-      forGivenLanguage('pt');
-      const text = 'Play bingo';
-
-      renderLink(text, { href: '/play/bingo' });
-
-      await thenLinkShouldHave(text, { href: '/jogar/bingo' });
+  beforeEach(() => {
+    const useTranslation = jest.spyOn(nextI18Next, 'useTranslation');
+    const useContext = jest.spyOn(React, 'useContext').mockReturnValue(
+      mockDeep<ReturnType<UseTranslation>>({ i18n: { options: { react: { useSuspense: false, wait: undefined } } } }),
+    );
+    afterEach(() => {
+      useTranslation.mockReset();
+      useContext.mockReset();
     });
+  });
 
-    it('does not change the url on invalid languages', async () => {
-      forGivenLanguage('invalid');
-      const text = 'Play bingo';
+  describe('Link', () => {
+    describe('when translated route is available', () => {
+      it('replaces the href property for it ', async () => {
+        forGivenLanguage('pt');
+        const text = 'Play bingo';
 
-      renderLink(text, { href: '/play/bingo' });
+        renderLink(text, { href: '/play/bingo' });
 
-      await thenLinkShouldHave(text, { href: '/play/bingo' });
-    });
+        await thenLinkShouldHave(text, { href: '/jogar/bingo' });
+      });
 
-    it('does not change url for non-translated path ', async () => {
-      forGivenLanguage('pt');
-      const text = 'Play bingo';
+      it('does not change the url on invalid languages', async () => {
+        forGivenLanguage('invalid');
+        const text = 'Play bingo';
 
-      renderLink(text, { href: '/unknown/path' });
+        renderLink(text, { href: '/play/bingo' });
 
-      await thenLinkShouldHave(text, { href: '/unknown/path' });
+        await thenLinkShouldHave(text, { href: '/play/bingo' });
+      });
+
+      it('does not change url for non-translated path ', async () => {
+        forGivenLanguage('pt');
+        const text = 'Play bingo';
+
+        renderLink(text, { href: '/unknown/path' });
+
+        await thenLinkShouldHave(text, { href: '/unknown/path' });
+      });
     });
   });
 });
 
 function forGivenLanguage(language: string) {
-  mockedNextI18Next.useTranslation.mockReturnValue({
-    i18n: { language },
-  } as ReturnType<typeof mockedNextI18Next.useTranslation>);
+  mocked<UseTranslation>(nextI18Next.useTranslation).mockReturnValue(
+    mock<ReturnType<UseTranslation>>({
+      i18n: { language },
+    }),
+  );
 }
 
 function renderLink(text: string, props: Pick<LinkProps, 'href'>) {
--- web/src/infra/i18n/config.ts
+++ web/src/infra/i18n/config.ts
@@ -2,6 +2,8 @@ import NextI18Next from 'next-i18next';
 import { resolve } from 'path';
 import { i18n, localeSubpaths } from '../../../next-i18next.config';
 
+export type INextI18Next = NextI18Next;
+
 export const nextI18Next = new NextI18Next({
   defaultLanguage: i18n.defaultLocale,
   otherLanguages: i18n.locales.filter((l) => l !== i18n.defaultLocale),
