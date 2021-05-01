685168974605ff12ca975b457ce7f38215fdb853
flamecoals <flamecoals@users.noreply.github.com>
2021-05-01 15:47:51 -0700

    Fix tests and env variable on deployment

--- web/cypress/integration/tablet/404.ts
+++ web/cypress/integration/tablet/404.ts
@@ -2,14 +2,14 @@ it('shows 404 page, clicks link to go home', () => {
   cy.visit('/play/foo', { failOnStatusCode: false });
   cy.contains('Page Not Found');
   cy.contains('Go Home').click();
-  cy.url().should('equal', Cypress.config().baseUrl + '/');
+  cy.url().should('equal', Cypress.config().baseUrl + '/en');
 });
 
 it('shows 404 page for unknown gamecode', () => {
   cy.visit('/play/foobarbaz', { failOnStatusCode: false });
   cy.contains('Page Not Found');
   cy.contains('Go Home').click();
-  cy.url().should('equal', Cypress.config().baseUrl + '/');
+  cy.url().should('equal', Cypress.config().baseUrl + '/en');
 });
 
 export {};
