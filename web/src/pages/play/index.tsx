11f171dcde9e501753a270249fe1a6d53abfc930
richard.lopes <richard.lopes92@gmail.com>
2021-04-30 22:11:03 -0300

    feat: fix reidrect over /play without game

--- web/src/pages/play/index.tsx
+++ web/src/pages/play/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Router from 'next/router';
+import { Router } from 'infra/i18n';
 
 export default class extends React.Component {
   static async getInitialProps({ res }) {
