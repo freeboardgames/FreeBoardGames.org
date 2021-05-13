c9d6f00d28d47d61f14b32b1c638d1ce25188d14
richard.lopes <richard.lopes92@gmail.com>
2021-05-13 15:15:03 -0300

    refactor: replace raw English text

--- web/src/pages/_error.tsx
+++ web/src/pages/_error.tsx
@@ -1,7 +1,15 @@
 /* eslint-disable react/react-in-jsx-scope */
 
 import MessagePage from 'infra/common/components/alert/MessagePage';
+import { useTranslation } from 'infra/i18n';
 
 export default function ErrorPage() {
-  return <MessagePage type={'error'} message={'Page Not Found'} />;
+  const { t } = useTranslation('ErrorPage');
+  return <MessagePage type={'error'} message={t('page_not_found')} />;
 }
+
+ErrorPage.getInitialProps = () => {
+  return {
+    namespacesRequired: ['ErrorPage'],
+  };
+};
