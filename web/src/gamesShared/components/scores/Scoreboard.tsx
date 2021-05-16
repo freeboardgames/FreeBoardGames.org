cf86a856ade1701cff7665b89ea9f27743c493f6
richard.lopes <richard.lopes92@gmail.com>
2021-05-16 11:10:49 -0300

    refactor: replace raw English text on Scoreboard

--- web/src/gamesShared/components/scores/Scoreboard.tsx
+++ web/src/gamesShared/components/scores/Scoreboard.tsx
@@ -1,11 +1,12 @@
-import * as React from 'react';
+import grey from '@material-ui/core/colors/grey';
 import Table from '@material-ui/core/Table';
-import TableRow from '@material-ui/core/TableRow';
-import TableHead from '@material-ui/core/TableHead';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
-import grey from '@material-ui/core/colors/grey';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
 import { IPlayerInRoom } from 'gamesShared/definitions/player';
+import { useTranslation } from 'infra/i18n';
+import React from 'react';
 
 export interface IScore {
   playerID: string;
@@ -19,38 +20,38 @@ interface IScoreboardProps {
   scoreName?: string;
 }
 
-export class Scoreboard extends React.Component<IScoreboardProps, {}> {
-  render() {
-    return (
-      <div className="scoreboard">
-        <Table>
-          <TableHead>
-            <TableRow>
-              <TableCell>Rank</TableCell>
-              <TableCell>Player</TableCell>
-              <TableCell>{this.props.scoreName || 'Score'}</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {this.props.scoreboard.map((score, i) => {
-              let style = {};
-              if (score.playerID.toString() === this.props.playerID) {
-                style = {
-                  background: grey[200],
-                };
-              }
-              const name = this.props.players.find((player) => player.playerID.toString() === score.playerID).name;
-              return (
-                <TableRow key={score.playerID} style={style}>
-                  <TableCell>#{i + 1}</TableCell>
-                  <TableCell>{name}</TableCell>
-                  <TableCell>{score.score}</TableCell>
-                </TableRow>
-              );
-            })}
-          </TableBody>
-        </Table>
-      </div>
-    );
-  }
+export function Scoreboard(props: IScoreboardProps) {
+  const { t } = useTranslation('Scoreboard');
+
+  return (
+    <div className="scoreboard">
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell>{t('rank')}</TableCell>
+            <TableCell>{t('player')}</TableCell>
+            <TableCell>{props.scoreName || t('score')}</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {props.scoreboard.map((score, i) => {
+            let style = {};
+            if (score.playerID.toString() === props.playerID) {
+              style = {
+                background: grey[200],
+              };
+            }
+            const name = props.players.find((player) => player.playerID.toString() === score.playerID).name;
+            return (
+              <TableRow key={score.playerID} style={style}>
+                <TableCell>#{i + 1}</TableCell>
+                <TableCell>{name}</TableCell>
+                <TableCell>{score.score}</TableCell>
+              </TableRow>
+            );
+          })}
+        </TableBody>
+      </Table>
+    </div>
+  );
 }
