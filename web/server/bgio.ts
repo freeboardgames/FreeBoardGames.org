80e32a622e723d6e04bbb34a31e50a7228335058
Vinny <vdfdev@users.noreply.github.com>
2021-05-16 22:35:25 -0700

    Start scaling bgio server

--- web/server/bgio.ts
+++ web/server/bgio.ts
@@ -5,20 +5,42 @@
 import { GAMES_LIST } from 'games';
 import noCache from 'koa-no-cache';
 const cors = require('@koa/cors'); // tslint:disable-line
-import { Server } from 'boardgame.io/server';
+import { Server, SocketIO } from 'boardgame.io/server';
 import { PostgresStore } from 'bgio-postgres';
+import { createAdapter } from '@socket.io/redis-adapter';
+import { RedisClient } from 'redis';
 
 const PORT = parseInt(process.env.BGIO_PORT || '8001', 10);
+
+function getDb() {
+  const pgUrl = process.env.POSTGRES_URL;
+  if (!pgUrl) {
+    return;
+  }
+  return new PostgresStore(pgUrl);
+}
+
+function getTransport() {
+  const host = process.env.FBG_REDIS_HOST;
+  const port = process.env.FBG_REDIS_PORT;
+  const user = process.env.FBG_REDIS_USER;
+  const password = process.env.FBG_REDIS_PASSWORD;
+  if (!host || !port || !password || !user) {
+    return;
+  }
+  const pubClient = new RedisClient({ host, port, user, password });
+  const subClient = pubClient.duplicate();
+  return new SocketIO({ socketAdapter: createAdapter(pubClient, subClient) });
+}
 
 const startServer = async () => {
   const configs = Promise.all(GAMES_LIST.map((gameDef) => gameDef.config()));
   const games = (await configs).map((config) => config.default.bgioGame);
-  const pgUrl = process.env.POSTGRES_URL;
-  const db = pgUrl ? new PostgresStore(pgUrl) : undefined;
-  const server = Server({ games, db });
+  const server = Server({ games, db: getDb(), transport: getTransport() });
   server.app.use(noCache({ global: true }));
   server.app.use(cors());
   server.run(PORT, () => {
+    // eslint-disable-next-line
     console.log(`Serving boardgame.io at: http://0.0.0.0:${PORT}/`); // tslint:disable-line
   });
 };
