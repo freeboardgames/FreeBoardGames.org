ef7ae29b4043078ea9a5184b0d911f6cee2b607e
richard.lopes <richard.lopes92@gmail.com>
2021-05-19 14:08:55 -0300

    customization: replace renderFull on MemoryMatch

--- web/src/games/memorymatch/customization.tsx
+++ web/src/games/memorymatch/customization.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { GameCustomization, GameCustomizationProps } from 'gamesShared/definitions/customization';
 import Checkbox from '@material-ui/core/Checkbox';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
-import { withCurrentGameTranslation, WithCurrentGameTranslation } from 'infra/i18n';
 
 export interface FullCustomizationState {
   stayInTurnOnMatch: boolean;
@@ -22,25 +21,25 @@ const changeStayInTurnOnMatch = (
   }
 };
 
-const FullCustomization = withCurrentGameTranslation(
-  ({ currentValue, onChange }: GameCustomizationProps & WithCurrentGameTranslation) => {
-    const state = (currentValue as FullCustomizationState) || DEFAULT_FULL_CUSTOMIZATION;
-    return (
-      <div style={{ padding: '16px' }}>
-        <FormControlLabel
-          control={
-            <Checkbox
-              checked={state.stayInTurnOnMatch}
-              onChange={changeStayInTurnOnMatch(onChange, state)}
-              color="primary"
-            />
-          }
-          label="Stay In Turn On Match"
-        />
-      </div>
-    );
-  },
-);
+type Props = GameCustomizationProps;
+
+const FullCustomization = ({ currentValue, onChange }: Props) => {
+  const state = (currentValue as FullCustomizationState) || DEFAULT_FULL_CUSTOMIZATION;
+  return (
+    <div style={{ padding: '16px' }}>
+      <FormControlLabel
+        control={
+          <Checkbox
+            checked={state.stayInTurnOnMatch}
+            onChange={changeStayInTurnOnMatch(onChange, state)}
+            color="primary"
+          />
+        }
+        label="Stay In Turn On Match"
+      />
+    </div>
+  );
+};
 
 const customization: GameCustomization = {
   FullCustomization,
