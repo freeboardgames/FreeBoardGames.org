9afddaacad2d85a5be308a037bc2380a7acc9988
richard.lopes <richard.lopes92@gmail.com>
2021-05-22 14:08:07 -0300

    refactor: replace raw English text on bingo

--- web/src/games/bingo/index.ts
+++ web/src/games/bingo/index.ts
@@ -1,22 +1,20 @@
 const Thumbnail = require('./media/thumbnail.jpg');
 import { GameMode } from 'gamesShared/definitions/mode';
-import { IGameDef, IGameStatus } from 'gamesShared/definitions/game';
-import instructions from './instructions.md';
+import { IGameDef, IGameStatus, IGameTranslationStatus } from 'gamesShared/definitions/game';
+import translation from './locales/en.json';
 
 export const BingoDef: IGameDef = {
   code: 'bingo',
-  name: 'Bingo',
+  name: translation.name,
+  translationStatus: { pt: IGameTranslationStatus.PARTIAL },
   contributors: ['gk-patel'],
   minPlayers: 2,
   maxPlayers: 30,
   imageURL: Thumbnail,
   modes: [{ mode: GameMode.OnlineFriend }],
-  description: 'Try your luck at Bingo!',
-  descriptionTag: 'Play Bingo for free online with upto 30 persons.',
-  instructions: {
-    videoId: 'dJ4YDTqmhJk',
-    text: instructions,
-  },
+  description: translation.description,
+  descriptionTag: translation.descriptionTag,
+  instructions: translation.instructions,
   config: () => import('./config'),
   status: IGameStatus.PUBLISHED,
 };
--- web/src/games/bingo/instructions.md
+++
@@ -1,3 +0,0 @@
-The objective of the game is to be the first player to get 5 in a row horizontally, vertically, or diagonally. Each player gets a card with numbers arranged in a 5x5 table, and is required to marker the announced number. The computer then calls out a number and each player then marks the called letter on their card if it is present. If a player has 5 in a row they call out “BINGO” (by pressing the shout button). 
-
-Image credits [Freepik](http://www.freepik.com)
