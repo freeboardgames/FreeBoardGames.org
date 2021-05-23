a236ac5c107fdfc0fd87ad387751cea36130f518
richard.lopes <richard.lopes92@gmail.com>
2021-05-23 14:58:19 -0300

    refactor: replace raw English text on soupofletters

--- web/src/games/soupofletters/index.ts
+++ web/src/games/soupofletters/index.ts
@@ -1,25 +1,21 @@
 const Thumbnail = require('./media/thumbnail.png');
 import { GameMode } from 'gamesShared/definitions/mode';
 import { IGameDef, IGameStatus } from 'gamesShared/definitions/game';
-import instructions from './instructions.md';
+import translation from './locales/en.json';
 
 export const soupOfLettersGameDef: IGameDef = {
   code: 'soupofletters',
-  name: 'Soup of Letters',
+  name: translation.name,
   contributors: ['gk-patel'],
   imageURL: Thumbnail,
   modes: [{ mode: GameMode.LocalFriend }, { mode: GameMode.OnlineFriend }],
   minPlayers: 2,
   maxPlayers: 4,
-  description: 'Crosswords with friends.',
-  descriptionTag: `Soup of letters, Alphabet soup, Crosswords. Find words in puzzle game.`,
-  instructions: {
-    // videoId: '_XVs7CmSXTw',
-    text: instructions,
-  },
+  description: translation.description,
+  descriptionTag: translation.descriptionTag,
+  instructions: translation.instructions,
   status: IGameStatus.PUBLISHED,
   config: () => import('./config'),
-  // aiConfig: () => import('./ai'),
 };
 
 export default soupOfLettersGameDef;
