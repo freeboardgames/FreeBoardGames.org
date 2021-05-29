ee1f1fe4213e0998002a096540c6cef91e47e20a
richard.lopes <richard.lopes92@gmail.com>
2021-05-29 10:31:25 -0300

    refactor: replace raw English text on secretcodes

--- web/src/games/secretcodes/index.ts
+++ web/src/games/secretcodes/index.ts
@@ -1,24 +1,19 @@
 const Thumbnail = require('./media/thumbnail.jpg');
 import { IGameDef, IGameStatus } from 'gamesShared/definitions/game';
 import { GameMode } from 'gamesShared/definitions/mode';
-import instructions from './instructions.md';
+import translation from './locales/en.json';
 
 export const secretcodesGameDef: IGameDef = {
   code: 'secretcodes',
-  name: 'Secret Codes',
+  name: translation.name,
   contributors: ['JvSomeren', 'mateusazis'],
   imageURL: Thumbnail,
   modes: [{ mode: GameMode.LocalFriend }, { mode: GameMode.OnlineFriend }],
   minPlayers: 4,
   maxPlayers: 20,
-  description: 'Similar to Codenames',
-  descriptionTag: `Play Secret Codes, a free online game\
- similar to the board game Codenames! You can play multi-player\
- from four and up to twenty players online!`,
-  instructions: {
-    videoId: 'zQVHkl8oQEU',
-    text: instructions,
-  },
+  description: translation.description,
+  descriptionTag: translation.descriptionTag,
+  instructions: translation.instructions,
   status: IGameStatus.PUBLISHED,
   config: () => import('./config'),
   customization: () => import('./customization'),
--- web/src/games/secretcodes/instructions.md
+++
@@ -1,5 +0,0 @@
-Secret codes is a game similar to codenames. The game has two teams, blue and red, and withing each team there is a leader.
-
-Only the leaders know which cards need to be selected by their teammates. However, leaders can only give a word and a number as a clue to their team
-
-The objective is for the rest of the team to figure out all their secret words before the competing team does. And they need to be careful, choosing a black card will lead to immediately losing the game!
