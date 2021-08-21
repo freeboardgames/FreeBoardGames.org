93c63f7d702e1927f15fb112f875fd8224c4cb27
ProspectPyxis <prospectpyxis@gmail.com>
2021-08-21 11:31:11 +0700

    Game setup code

--- web/src/games/picnicGo/game.ts
+++ web/src/games/picnicGo/game.ts
@@ -1,9 +1,48 @@
-// import { IG } from './types'
+import { Ctx } from 'boardgame.io';
+import { IG } from './types';
+import { defaultDeck } from './cards';
+
+export function setupRound(g: IG, ctx: Ctx): IG {
+  let deck = ctx.random.Shuffle(defaultDeck);
+
+  let hands = new Array(ctx.numPlayers).fill(0).map((_, i) => ({
+    currentOwner: i,
+    hand: new Array(12 - ctx.numPlayers).fill(0).map(() => {
+      const card = deck.pop();
+      return card;
+    }),
+  }));
+
+  for (let i = 0; i < ctx.numPlayers; i++) {
+    g.players[i].playedCards = [];
+  }
+
+  return {
+    deck,
+    players: g.players,
+    hands,
+  };
+}
 
 export const PicnicGoGame = {
-  name: 'foobar',
+  name: 'picnicGo',
 
-  setup: () => ({ count: 0 }),
+  setup: (ctx) => {
+    const baseState = {
+      deck: defaultDeck,
+      players: new Array(ctx.numPlayers).fill({
+        playedCards: [],
+        score: 0,
+        dessertsCount: 0,
+        chipsCount: 0,
+        unusedMayo: 0,
+        unusedForks: 0,
+      }),
+      hands: [{ currentOwner: 0, hand: [] }],
+    };
+
+    return setupRound(baseState, ctx);
+  },
 
   moves: {},
 
