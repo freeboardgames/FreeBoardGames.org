17ad7939aa9c67178692eab54ea00b3fc42d3a09
ProspectPyxis <prospectpyxis@gmail.com>
2021-08-21 09:25:38 +0700

    Add types file

---
+++ web/src/games/picnicGo/types.ts
@@ -0,0 +1,31 @@
+export interface IG {
+  deck: string[];
+  player: IPlayer[];
+  hands: IHand[];
+}
+
+export const enum cardTypes {
+  sandwich,
+  chips,
+  meals,
+  special,
+  dessert,
+}
+
+export interface ICardDefinition {
+  name: string;
+  cardType: cardTypes;
+  scoreFunc(p: IPlayer): IPlayer;
+}
+
+export interface IPlayer {
+  playedCards: string[];
+  score: number;
+  dessertsPlayed: number;
+  chipsCount: number;
+}
+
+export interface IHand {
+  currentOwner: number;
+  hand: string[];
+}
