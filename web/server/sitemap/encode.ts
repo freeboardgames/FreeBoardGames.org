f6548e082702713a87b82bbdad8bc8161480d3d9
vdf.dev <vdfdev@users.noreply.github.com>
2021-08-22 20:23:03 -0700

    Fix tag xhtml:link

--- web/server/sitemap/encode.ts
+++ web/server/sitemap/encode.ts
@@ -2,11 +2,11 @@ import { UrlTag, AlternateLink } from './types';
 
 export const encodeSitemap = (urls: UrlTag[], host: string) => {
   const xml = buildTemplateWith(urls, host);
-  return sanitize(xml);
+  return xml;
 };
 
 const buildAlternateLinkWith = (host: string) => (alternateLink: AlternateLink) => {
-  return `<link rel="alternate" hreflang="${alternateLink.hreflang}" href="${host}${alternateLink.href}" />`;
+  return `<xhtml:link rel="alternate" hreflang="${alternateLink.hreflang}" href="${host}${alternateLink.href}" />`;
 };
 
 const buildUrlWith = (host: string) => (url: UrlTag): string => {
@@ -25,12 +25,4 @@ function buildTemplateWith(urls: UrlTag[], host: string): string {
   <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
     ${urls.map(buildUrlWith(host)).join('\n')}
   </urlset>`;
-}
-
-function sanitize(xml: string) {
-  return xml
-    .replace(/(\r\n|\n|\r)/gm, '')
-    .replace(/>\s*/g, '>')
-    .replace(/\s*</g, '<')
-    .trim();
 }
