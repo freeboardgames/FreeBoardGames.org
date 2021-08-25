3374d00ada0e732bbf166dad56cb973cf4a32970
vdf.dev <vdfdev@users.noreply.github.com>
2021-08-24 23:12:56 -0700

    Uses youtube facade instead of the youtube player to save some MBs of download

--- web/src/infra/gameInfo/GameInstructionsVideo.tsx
+++ web/src/infra/gameInfo/GameInstructionsVideo.tsx
@@ -1,36 +1,24 @@
 import React from 'react';
 import Card from '@material-ui/core/Card';
+import LiteYouTubeEmbed from 'react-lite-youtube-embed';
+import { useTranslation } from 'infra/i18n';
+import 'react-lite-youtube-embed/dist/LiteYouTubeEmbed.css';
 
 interface IGameInstructionsProps {
   videoId: string;
 }
 
-export class GameInstructionsVideo extends React.Component<IGameInstructionsProps, {}> {
-  render() {
-    return (
-      <Card style={{ marginBottom: 16, maxWidth: '1200px' }}>
-        <div
-          style={{
-            position: 'relative',
-            maxWidth: '1200px',
-            maxHeight: '360px',
-            paddingBottom: '56.25%',
-            paddingTop: 30,
-            height: 0,
-            overflow: 'hidden',
-          }}
-        >
-          <iframe
-            data-testid={'gameinstructionsvideo'}
-            style={{ width: '100%', height: '100%', position: 'absolute', top: 0, left: 0 }}
-            width="500"
-            height="360"
-            src={`https://www.youtube.com/embed/${this.props.videoId}`}
-            allowFullScreen
-            frameBorder="0"
-          />
-        </div>
-      </Card>
-    );
-  }
-}
+export const GameInstructionsVideo = (props: IGameInstructionsProps) => {
+  const { t } = useTranslation('GameInfo');
+  return (
+    <Card style={{ marginBottom: 16, maxWidth: '1200px' }} data-testid={'gameinstructionsvideo'}>
+      <LiteYouTubeEmbed
+        id={props.videoId}
+        announce={t('watch')}
+        title={t('video_instructions')}
+        adNetwork={false}
+        noCookie={true}
+      />
+    </Card>
+  );
+};
