9bc1ee45e78ff3530285da2c25108daa4c6b3d0a
vdf.dev <vdfdev@users.noreply.github.com>
2021-09-09 16:04:05 -0700

    Fix translation

--- web/src/infra/lobby/NewRoomCard.tsx
+++ web/src/infra/lobby/NewRoomCard.tsx
@@ -1,18 +1,20 @@
 import css from './NewRoomCard.module.css';
 import AddIcon from '@material-ui/icons/Add';
 import { Typography } from '@material-ui/core';
+import { useTranslation } from 'infra/i18n/hooks/useTranslation';
 
 interface NewRoomCardProps {
   newRoomModal: () => void;
 }
 
 export const NewRoomCard = ({ newRoomModal }: NewRoomCardProps) => {
+  const { t } = useTranslation('LobbyCarousel');
   return (
     <div className={css.CardWrapper} data-testid={`newRoom`} onClick={newRoomModal}>
       <div className={css.InnerWrapper}>
         <AddIcon fontSize="large" />
         <Typography component="div" variant="subtitle1">
-          New Room
+          {t('new_room')}
         </Typography>
       </div>
     </div>
