eed2ed13889ec8ceba4bd1fcfb11bf46c225384f
ProspectPyxis <prospectpyxis@gmail.com>
2021-09-19 11:36:18 +0700

    Add game over display

--- web/src/games/picnicGo/board.tsx
+++ web/src/games/picnicGo/board.tsx
@@ -1,6 +1,8 @@
 import * as React from 'react';
 import { IGameArgs } from 'gamesShared/definitions/game';
 import { GameLayout } from 'gamesShared/components/fbg/GameLayout';
+import { IScore, Scoreboard } from 'gamesShared/components/scores/Scoreboard';
+import { isSpectator } from 'gamesShared/helpers/GameUtil';
 import { Ctx } from 'boardgame.io';
 import { IG } from './types';
 import { BottomInfo } from './components/BottomInfo';
@@ -17,6 +19,16 @@ interface IBoardProps {
 
 export class Board extends React.Component<IBoardProps, {}> {
   render() {
+    if (this.props.ctx.gameover) {
+      return (
+        <GameLayout
+          gameOver={this._getGameOver()}
+          extraCardContent={this._getScoreboard()}
+          gameArgs={this.props.gameArgs}
+        />
+      );
+    }
+
     return (
       <GameLayout gameArgs={this.props.gameArgs} maxWidth="750px">
         <Typography variant="h5" style={{ textAlign: 'center', color: 'white', marginBottom: '8px' }}>
@@ -25,6 +37,32 @@ export class Board extends React.Component<IBoardProps, {}> {
         {this._getPlayersInfo()}
         <BottomInfo playerID={this.props.playerID} G={this.props.G} ctx={this.props.ctx} />
       </GameLayout>
+    );
+  }
+
+  _getGameOver() {
+    const scoreboard: IScore[] = this.props.ctx.gameover.scoreboard;
+    if (scoreboard[0].score === scoreboard[scoreboard.length - 1].score) {
+      return 'draw';
+    } else {
+      if (isSpectator(this.props.playerID)) {
+        return 'see scoreboard';
+      }
+      if (scoreboard[0].score === scoreboard.find((e) => e.playerID === this.props.playerID).score) {
+        return 'you won';
+      } else {
+        return 'you lost';
+      }
+    }
+  }
+
+  _getScoreboard() {
+    return (
+      <Scoreboard
+        scoreboard={this.props.ctx.gameover.scoreboard}
+        playerID={this.props.playerID}
+        players={this.props.gameArgs.players}
+      />
     );
   }
 
