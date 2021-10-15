6eccc5b7c7dd998e702802e7ffa01a6678a66ffd
Charles Watson <charles.watson@acm.org>
2021-10-14 21:01:13 -0700

    first working version

--- web/.storybook/preview.js
+++ web/.storybook/preview.js
@@ -5,11 +5,14 @@ import i18n from 'i18next';
 addDecorator(i18nextDecorator());
 
 function i18nextDecorator() {
-  return (Story) => (
-    <I18nextProvider i18n={i18n}>
+
+  return (Story, context) => {
+    if (context.globals.locale) { i18n.changeLanguage(context.globals.locale) };
+    return (<I18nextProvider i18n={i18n}>
       <Story />
     </I18nextProvider>
-  );
+    )
+  };
 }
 
 // .storybook/preview.js
