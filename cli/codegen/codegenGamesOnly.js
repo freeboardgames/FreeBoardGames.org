9c38c439205663527a1a32b8bc54ed585f92e923
Vinny <1034631+vdfdev@users.noreply.github.com>
2022-03-24 20:30:07 -0700

    Update codegenGamesOnly.js

--- cli/codegen/codegenGamesOnly.js
+++ cli/codegen/codegenGamesOnly.js
@@ -2,5 +2,5 @@ const { genGames } = require("./genGames.js");
 const { fbgRun } = require("../util");
 
 genGames([]);
-fbgRun("yarn install ts-node cross-env glob");
+fbgRun("yarn add ts-node cross-env glob");
 fbgRun("yarn run i18n:copy");
