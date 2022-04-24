3bef50906c8c6b1bd2e789ced2f7f24f3c2d0728
vdf.dev <me@vdf.dev>
2022-04-23 21:49:16 -0700

    Fix node-fetch

--- web/src/test/setup/mockFetch.ts
+++ web/src/test/setup/mockFetch.ts
@@ -1,6 +1,7 @@
 // import `fetch` to fix apollo-link-http-common's Invariant Violation:
 // fetch is not found globally and no fetcher passed, to fix pass a fetch for
 // your environment like https://www.npmjs.com/package/node-fetch.
-(global as any).fetch = require('node-fetch');
+import fetch from 'node-fetch';
+(global as any).fetch = fetch;
 
 export {};
