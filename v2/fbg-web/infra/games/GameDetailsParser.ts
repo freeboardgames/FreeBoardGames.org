68a59f55d680f4981cac87f510b042870c3e6e9b
vdf.dev <me@vdf.dev>
2022-06-01 22:24:52 -0700

    [v2] Adds game details page content

---
+++ v2/fbg-web/infra/games/GameDetailsParser.ts
@@ -0,0 +1,82 @@
+import YAML from 'yaml';
+
+export enum GameMode {
+  OnlineFriend,
+  LocalFriend,
+  AI
+}
+
+export interface GameInstructions {
+  text: string;
+  videoId?: string;
+}
+
+export interface GameDetails {
+  contributors: string[];
+  instructions: GameInstructions;
+  modes: GameMode[];
+}
+
+const parseGameInstructions = function(gameDef: any, lang:string, gameId: string) {
+  const i18nGame = gameDef[lang];
+  if (!('instructions' in i18nGame)) {
+    throw new Error(`instructions configuration missing for ${gameId} and ${lang}`);
+  }
+  const text = i18nGame.instructions.text;
+  if (typeof text !== 'string' || text.length === 0) {
+    throw new Error(`Invalid instruction text for ${gameId} and ${lang}`); 
+  }
+  return {
+    text,
+    videoId: i18nGame.instructions.videoId
+  }
+}
+
+const parseGameContributors = function(gameDef: any, gameId: string): string[] {
+  if (!('contributors' in gameDef)) {
+    throw new Error(`contributors configuration missing for ${gameId}`);
+  }
+  const result = [];
+  for (const contributor of gameDef.contributors) {
+      if (typeof contributor !== 'string') {
+        throw new Error(`contributors usernames need to be a string for ${gameId}`); 
+      }
+      result.push(contributor);
+  }
+  return result;
+}
+
+const parseGameModes = function(gameDef: any, gameId: string): GameMode[] {
+  if (!('modes' in gameDef)) {
+    throw new Error(`modes configuration missing for ${gameId}`);
+  }
+  const result = [];
+  for (const rawMode of gameDef['modes']) {
+    switch (rawMode) {
+        case 'OnlineFriend':
+            result.push(GameMode.OnlineFriend);
+            break;
+        case 'LocalFriend':
+            result.push(GameMode.LocalFriend);
+            break;
+        case 'AI':
+            result.push(GameMode.AI);
+            break;
+        default:
+            throw new Error(`Invalid game mode "${rawMode}" on ${gameId}`);
+    }
+  }
+  return result;
+}
+
+export const parseGameDetails = function(gameYaml: string, lang: string, gameId: string): GameDetails {
+  const gameDef = YAML.parse(gameYaml);
+  const modes = parseGameModes(gameDef, gameId); 
+  const contributors = parseGameContributors(gameDef, gameId);
+  const instructions = parseGameInstructions(gameDef, lang, gameId);
+  return {
+    modes,
+    contributors,
+    instructions
+  };
+}
--- v2/fbg-web/pages/[lang]/[playVerb]/[gameCode]/index.tsx
+++ v2/fbg-web/pages/[lang]/[playVerb]/[gameCode]/index.tsx
@@ -4,37 +4,92 @@ import { loadGameYaml } from '../../../../infra/games/GameLoader';
 import { parseGameTranslations } from '../../../../infra/games/GameTranslationsParser';
 import { loadI18nConfig } from '../../../../infra/i18n/I18nConfigLoader';
 import { parseI18nConfig } from '../../../../infra/i18n/I18nConfigParser';
-import { parseGameSummary } from '../../../../infra/games/GameSummaryParser';
+import { GameSummary, parseGameSummary } from '../../../../infra/games/GameSummaryParser';
+import { GameDetails, GameInstructions, GameMode, parseGameDetails } from '../../../../infra/games/GameDetailsParser';
+import { ReactElement } from 'react';
 
 interface GameInfoProps { 
-  lang: string;
-  playVerb: string;
-  gameCode: string;
+  summary: GameSummary;
+  details: GameDetails;
 }
 
 interface UrlParams {
   lang: string;
   playVerb: string;
   gameCode: string;
+  gameId: string;
 }
 
 interface UrlPath {
   params: UrlParams;
 }
 
-const GameInfo: NextPage<GameInfoProps> = function(props: GameInfoProps) {
+const gameInstructionToCard = function(instructions: GameInstructions): ReactElement {
+  let video = null;
+  const videoId = instructions.videoId;
+  if (videoId) {
+    video = <p>
+      <a href={`https://www.youtube.com/watch?v=${videoId}`} target="_blank">
+        Watch video
+      </a>
+    </p>;
+  }
   return (
     <>
-      <h1>Hello world!</h1>
-      <p>lang: {props.lang}</p>
-      <p>playVerb: {props.playVerb}</p>
-      <p>gameCode: {props.gameCode}</p>
+      <p>{instructions.text}</p>
+      {video}
+    </>
+  );
+}
+
+const gameModeToCard = function(mode: GameMode): ReactElement {
+  switch (mode) {
+    case GameMode.AI: 
+      return <li>AI: <a href="#">Play!</a></li>;
+    case GameMode.OnlineFriend: 
+      return <li>Online Friend: <a href="#">Play!</a></li>;
+    case GameMode.LocalFriend: 
+      return <li>Local Friend: <a href="#">Play!</a></li>;
+  }
+}
+
+const GameInfo: NextPage<GameInfoProps> = function(props: GameInfoProps) {
+  const modeCards = props.details.modes.map(gameModeToCard);
+  const instructions = gameInstructionToCard(props.details.instructions);
+  const contributors = props.details.contributors.map((contributor) => (
+    <li>
+      <a href={`https://github.com/${contributor}`} target="_blank">
+        {contributor}
+      </a>
+    </li>
+  ));
+  return (
+    <>
+      <h1>Play {props.summary.name}</h1>
+      <div>
+        <h2>Game Modes</h2>
+        <ul>{modeCards}</ul>
+      </div>
+      <div>
+        <h2>Instructions</h2>
+        {instructions}
+      </div>
+      <div>
+        <h2>Contributors</h2>
+        <ul>
+          {contributors}
+        </ul>
+      </div>
     </>
   )
 }
 
 export async function getStaticProps(path: UrlPath): Promise<{ props: GameInfoProps }> {
-  return { props: path.params };
+  const { lang, gameCode } = path.params;
+  const gameYaml = await loadGameYaml(gameCode);
+  const summary = parseGameSummary(gameYaml, lang, gameCode);
+  const details = parseGameDetails(gameYaml, lang, gameCode);
+  return { props: { summary, details } };
 }
 
 export async function getStaticPaths() {
@@ -48,7 +103,7 @@ export async function getStaticPaths() {
       const gameSummary = parseGameSummary(gameYaml, lang, gameId);
       const gameCode = gameSummary.code;
       const playVerb = i18nConfig[lang].playVerb;
-      paths.push({ params: { lang, playVerb, gameCode } });
+      paths.push({ params: { lang, playVerb, gameCode, gameId } });
     }
   } 
   return { paths, fallback: false };
