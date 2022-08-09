cf668caf21b4ecadfb2dbf90f727296a264b25b4
vdfdev <me@vdf.dev>
2022-08-08 23:06:17 -0700

    Adds SEO and removes dead code

--- v2/fbg-web/pages/[lang]/[playVerb]/[gameCode]/index.tsx
+++ v2/fbg-web/pages/[lang]/[playVerb]/[gameCode]/index.tsx
@@ -3,18 +3,14 @@ import { loadGameYaml } from "infra/games/GameLoader";
 import { loadI18nConfig } from "infra/i18n/I18nConfigLoader";
 import { parseI18nConfig } from "infra/i18n/I18nConfigParser";
 import { GameSummary, parseGameSummary } from "infra/games/GameSummaryParser";
-import { GameMode } from "fbg-games/gamesShared/definitions/mode";
 import {
   GameDetails,
-  GameInstructions,
   parseGameDetails,
 } from "infra/games/GameDetailsParser";
-import { ReactElement } from "react";
 import { getGameIdFromCode } from "infra/i18n/I18nGetGameId";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import { useTranslation } from "next-i18next";
 import { listGameSummaries } from "infra/games/ListGameSummaries";
-import Link from "next/link";
 import fs from "fs";
 import path from "path";
 import { FreeBoardGamesBar } from 'fbg-games/gamesShared/components/fbg/FreeBoardGamesBar';
@@ -23,11 +19,15 @@ import Typography from '@mui/material/Typography';
 import Alert from '@mui/material/Alert';
 import css from './index.module.css';
 import { BreadcrumbJsonLd } from 'next-seo';
+import SEO from 'fbg-web/infra/misc/SEO';
+import { parseGameTranslations, GameTranslations, GameTranslationStatus } from "fbg-web/infra/games/GameTranslationsParser";
+import { GameCard } from 'fbg-web/infra/widgets/GameCard';
 
 interface GameInfoProps {
   summary: GameSummary;
   details: GameDetails;
   urlParams: UrlParams;
+  translations: GameTranslations;
 }
 
 interface UrlParams {
@@ -38,76 +38,12 @@ interface UrlParams {
 }
 
 interface UrlPath {
-  params: UrlParams;
-}
-
-const gameInstructionToCard = function (
-  instructions: GameInstructions
-): ReactElement {
-  let video = null;
-  const videoId = instructions.videoId;
-  if (videoId) {
-    video = (
-      <p>
-        <a
-          href={`https://www.youtube.com/watch?v=${videoId}`}
-          rel="noreferrer"
-          target="_blank"
-        >
-          Watch video
-        </a>
-      </p>
-    );
-  }
-  return (
-    <>
-      <p>{instructions.text}</p>
-      {video}
-    </>
-  );
-};
-
-const gameModeToCard = function (
-  params: UrlParams,
-  mode: GameMode
-): ReactElement {
-  const baseUrl = `/${params.lang}/${params.playVerb}/${params.gameCode}`;
-  switch (mode) {
-    case GameMode.AI:
-      return (
-        <li>
-          AI:{" "}
-          <Link href={`${baseUrl}/ai`}>
-            <a>Play!</a>
-          </Link>
-        </li>
-      );
-    case GameMode.OnlineFriend:
-      return (
-        <li>
-          Online Friend:{" "}
-          <Link href={`${baseUrl}/online`}>
-            <a>Play!</a>
-          </Link>
-        </li>
-      );
-    case GameMode.LocalFriend:
-      return (
-        <li>
-          Local Friend:{" "}
-          <Link href={`${baseUrl}/local`}>
-            <a>Play!</a>
-          </Link>
-        </li>
-      );
-  }
-};
+  params: UrlParams;}
 
 const GameInfo: NextPage<GameInfoProps> = function (props: GameInfoProps) {
   const { t } = useTranslation("GameInfo");
-  const modeCards = props.details.modes.map((mode) =>
-    gameModeToCard(props.urlParams, mode)
-  );
+
+  const isFullyTranslated = props.translations[props.summary.lang] === GameTranslationStatus.DONE;
   const instructions = props.details.instructions;
   const gameVideoInstructions = instructions.videoId ? <GameInstructionsVideo videoId={instructions.videoId} /> : null;
 
@@ -145,9 +81,9 @@ const GameInfo: NextPage<GameInfoProps> = function (props: GameInfoProps) {
             <GameModePicker gameDef={gameDef} />
           </div>
           <div className={css.RightCol}>
-            <GameCard game={gameDef} />
+            <GameCard gameSummary={props.summary} gameTranslations={props.translations}  />
             <div style={{ marginTop: '16px' }}>
-              {!isFullyTranslated(gameDef) && (
+              {!isFullyTranslated && (
                 <Alert severity="warning">
                   <Trans
                     t={t}
@@ -173,7 +109,7 @@ const GameInfo: NextPage<GameInfoProps> = function (props: GameInfoProps) {
         </div>
       </DesktopView>
       <MobileView>
-        <GameCard game={gameDef} />
+        <GameCard gameSummary={props.summary} gameTranslations={props.translations}  />
         <div style={{ padding: '8px' }} data-testid={'MobileViewDiv'}>
           <Typography variant="h5" component="h1">
             {playTitle}
@@ -196,10 +132,12 @@ export async function getStaticProps(
   const gameYaml = await loadGameYaml(gameId);
   const summary = parseGameSummary(gameYaml, lang, gameCode);
   const details = parseGameDetails(gameYaml, lang, gameCode);
+  const gameTranslations = parseGameTranslations(gameYaml, gameId);
   return {
     props: {
       summary,
       details,
+      gameTranslations,
       urlParams: path.params,
       ...(await serverSideTranslations(lang, ["GameInfo"])),
     },
