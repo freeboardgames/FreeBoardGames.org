467666e7f05f906bac6381a439190066f8a9ceef
Poincare <iampengkeyao@gmail.com>
2022-10-17 05:08:05 +0200

    add more preload opening (#1851)

--- web/src/games/kriegspiel/Board.tsx
+++ web/src/games/kriegspiel/Board.tsx
@@ -76,7 +76,7 @@ export const Board = ({ G, ctx, moves, isActive, events, ...props }: GameProps &
           pickUpID(null);
           break;
         default:
-          if (pickedData(pickedID) !== null) {
+          if (pickedData(pickedID) !== null && canPut(G, ctx, pickedID, id)) {
             pickUpID(null);
             moves.movePiece(pickedID, id);
           } else {
@@ -342,30 +342,21 @@ export const Board = ({ G, ctx, moves, isActive, events, ...props }: GameProps &
     );
 
     return (
-      <table>
-        {obj?.belong === opponentID ? (
-          <>
-            <tr>
-              {eDefTd}
-              {myOffTd}
-            </tr>
-            <tr>
-              {eOffTd}
-              {myDefTd}
-            </tr>
-          </>
-        ) : (
-          <>
-            <tr>
-              {myDefTd}
-              {eOffTd}
-            </tr>
-            <tr>
-              {myOffTd}
-              {eDefTd}
-            </tr>
-          </>
-        )}
+      <table style={{ marginLeft: 'auto', marginRight: 'auto' }}>
+        {obj?.belong !== opponentID ? (
+          // if choose my unit or empty place
+          <tr>
+            {myDefTd}
+            {eOffTd}
+          </tr>
+        ) : null}
+        {obj?.belong !== myID ? (
+          // if choose enemy unit or empty place
+          <tr>
+            {myOffTd}
+            {eDefTd}
+          </tr>
+        ) : null}
       </table>
     );
   }
@@ -410,13 +401,13 @@ export const Board = ({ G, ctx, moves, isActive, events, ...props }: GameProps &
       {/* turn info */}
       <p>
         {spanBGColor(<>It is {currentPlayer === myID ? 'my' : "opponent's"} turn.</>, fictionColor(currentPlayer))}
-        <button disabled={!isActive} onClick={props.undo}>
-          Undo
-        </button>
         <button
           disabled={!isActive}
           onClick={() => {
-            events.endTurn && events.endTurn();
+            let text = 'End Turn?';
+            if (confirm(text)) {
+              events.endTurn && events.endTurn();
+            }
           }}
         >
           End Turn
@@ -424,7 +415,7 @@ export const Board = ({ G, ctx, moves, isActive, events, ...props }: GameProps &
       </p>
 
       {/* action info */}
-      <label title="Click below to undo.">My Moves and Attack:</label>
+      <label>My Moves and Attack:</label>
       <svg viewBox="-0.1 -0.1 6.2 1.2" onClick={props.undo} cursor="pointer">
         {renderLayer((_, id) => {
           const moveEdRec = G.moveRecords[myID].map((p) => p[1]);
@@ -456,6 +447,7 @@ export const Board = ({ G, ctx, moves, isActive, events, ...props }: GameProps &
           }
         }, Array(6).fill(null))}
       </svg>
+      <label>(click above to undo)</label>
       {/* retreat info */}
       {G.forcedRetreat[currentPlayer][0] !== null && <p>🏃‍♂️💥 I must retreat my unit from attack first.</p>}
 
@@ -521,7 +513,8 @@ export const Board = ({ G, ctx, moves, isActive, events, ...props }: GameProps &
           })(pickedID)}
       </p>
 
-      {/* battle factor */}
+      {/* combat factors */}
+      <label>Total Combat Factors:</label>
       {battleFactorTable(pickedID)}
     </div>
   );
@@ -588,6 +581,13 @@ export const Board = ({ G, ctx, moves, isActive, events, ...props }: GameProps &
       {/* Game Data */}
       <form>
         <label>GameData:</label>
+        <select onChange={(e) => setGameData(e.target.value)}>
+          {Game.gameList.map((option) => (
+            <option key={option.name} value={option.data}>
+              {option.name}
+            </option>
+          ))}
+        </select>
         <textarea
           name="gameData"
           id="gameData"
